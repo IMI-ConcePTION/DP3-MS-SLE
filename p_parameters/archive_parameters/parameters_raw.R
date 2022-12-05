a3bf74d2826422a712d30eb5753d0641a08f0d59
RosaMGini <rosa.gini@ars.toscana.it>
2022-12-05 18:53:55 +0100

    included manually diagnostic and medicines codes for MS algorithms (from MS SAP v4)

--- p_parameters/archive_parameters/parameters_raw.R
+++ p_parameters/archive_parameters/parameters_raw.R
@@ -1,4 +1,32 @@
 #--------------------------
+# Multiple sclerosis (MS)
+
+concept_set_codes_our_study_pre[["MS"]][["ICD9CM"]] <- c("340")
+concept_set_codes_our_study_pre[["MS"]][["ICD10"]] <- c("G35")
+concept_set_codes_our_study_pre[["MS"]][["ICPC2"]] <- c("F20..00", "F203.00", "F20..11", "F20z.00", "666A.00", "9kG..00", "9mD..", "9mD0.", "9mD1.", "666B.00", "8CS1.00", "8IAb.", "F202.00", "F207.00", "F200.00", "F208.00", "F201.00", "F204.00", "F206.00", "8Cc2.00", "8Cc1.00", "8Cc4.00", "8Cc0.00", "ZRVE.00", "8Cc3.00")
+
+
+#--------------------------
+# MS-specific DMT (DMT-MS_SPEC)
+
+concept_set_codes_our_study_pre[["DMT-MS_SPEC"]][["ATC"]] <- c("L04AA34", "L04AC01", "N07XX09", "L04AX07", "L04AA27", "L03AX13", "L03AB07", "L03AB08", "L04AA23", "L04AA36", "L03AB13", "L04AA31", "L01XC10", "L04AA42")
+
+#--------------------------
+# MS-non-specific DMT (DMT-MS_UNSPEC)
+
+concept_set_codes_our_study_pre[["DMT-MS_UNSPEC"]][["ATC"]] <- c("L04AX01", "L04AA40", "L01DB07", "L01XC02", "L04AX03", "L01AA01", "L04AA13", "L04AA06", "L04AD02")
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------
 # COVID_vaccine
 
 concept_set_codes_our_study_pre[["Covid_vaccine"]][["ATC"]] <- c("J07BX03")
