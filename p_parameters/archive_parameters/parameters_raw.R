e7de9620a3d574555843d6b07f890ab3e048cd00
RosaMGini <rosa.gini@ars.toscana.it>
2022-12-07 10:29:29 +0100

    reclassified Natalizumab (ATC = L04AA23) as non-specific drug

--- p_parameters/archive_parameters/parameters_raw.R
+++ p_parameters/archive_parameters/parameters_raw.R
@@ -10,10 +10,10 @@ concept_set_codes_our_study_pre[["MS"]][["READ"]] <- c("F20..00", "F203.00", "F2
 #--------------------------
 # MS-specific DMT (DMT-MS_SPEC)
 
-concept_set_codes_our_study_pre[["DMT-MS_SPEC"]][["ATC"]] <- c("L04AA34", "L04AC01", "N07XX09", "L04AX07", "L04AA27", "L03AX13", "L03AB07", "L03AB08", "L04AA23", "L04AA36", "L03AB13", "L04AA31", "L01XC10", "L04AA42")
+concept_set_codes_our_study_pre[["DMT-MS_SPEC"]][["ATC"]] <- c("L04AA34", "L04AC01", "N07XX09", "L04AX07", "L04AA27", "L03AX13", "L03AB07", "L03AB08", "L04AA36", "L03AB13", "L04AA31", "L01XC10", "L04AA42")
 
 #--------------------------
 # MS-non-specific DMT (DMT-MS_UNSPEC)
 
-concept_set_codes_our_study_pre[["DMT-MS_UNSPEC"]][["ATC"]] <- c("L04AX01", "L04AA40", "L01DB07", "L01XC02", "L04AX03", "L01AA01", "L04AA13", "L04AA06", "L04AD02")
+concept_set_codes_our_study_pre[["DMT-MS_UNSPEC"]][["ATC"]] <- c("L04AA23", "L04AX01", "L04AA40", "L01DB07", "L01XC02", "L04AX03", "L01AA01", "L04AA13", "L04AA06", "L04AD02")
 
