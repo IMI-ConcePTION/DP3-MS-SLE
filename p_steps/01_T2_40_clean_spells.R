13b195627dca264e39116c225f500f83beafc9bf
Davide <davide.messina@ars.toscana.it>
2022-12-19 18:05:21 +0100

    fixing cleaning of spells

--- p_steps/01_T2_40_clean_spells.R
+++ p_steps/01_T2_40_clean_spells.R
@@ -12,14 +12,12 @@ person_spell <- merge(D3_output_spells_category, D3_PERSONS, all.x = T, by = "pe
 person_spell <- person_spell[, .(person_id, birth_date, death_date, entry_spell_category_crude = entry_spell_category,
                                  exit_spell_category_crude = exit_spell_category, op_meaning, num_spell)]
 
-person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_spell_category_crude - 60,
-                                                           entry_spell_category_crude,
-                                                           birth_date)]
+person_spell[, entry_spell_category := entry_spell_category_crude]
 person_spell[, exit_spell_category := pmin(exit_spell_category_crude, death_date, na.rm = T)]
 
 person_spell[, op_start_date_cleaned := data.table::fifelse(entry_spell_category != entry_spell_category_crude, 0, 1)]
 person_spell[, op_end_date_cleaned := data.table::fifelse(exit_spell_category != exit_spell_category_crude, 0, 1)]
-person_spell[, starts_at_birth := data.table::fifelse(entry_spell_category == birth_date, 1, 0)]
+
 person_spell[, starts_after_ending := data.table::fifelse(entry_spell_category < exit_spell_category, 0, 1)]
 
 # find spells that do not overlap the study period
@@ -28,20 +26,16 @@ person_spell[, no_overlap_study_period := fifelse(
 
 # find spells that are shorter than x days
 person_spell[, spell_less_than_12_months_fup := fifelse(
-  correct_difftime(pmin(exit_spell_category, study_end), entry_spell_category) <= min_spell_lenght, 1, 0)]
+  correct_difftime(pmin(exit_spell_category, study_end),
+                   pmax(study_start, entry_spell_category)) <= min_spell_lenght, 1, 0)]
 
 
 #TODO
 #add additional criteria specific for the study (for example keep only the first spell for vaccinated)
 
-
-person_spell[starts_after_ending == 0 & no_overlap_study_period == 0 &
-               less_than_12_months_fup == 0 ,
-             flag := 0]
-
-
-#add a criteria that identify the specific spell of interest
-person_spell[flag==0 & entry_spell_category >= study_start & exit_spell_category<= study_start , is_the_study_spell := 1]
+# add a criteria that identify the specific spell of interest
+person_spell[starts_after_ending == 0 & no_overlap_study_period == 0 & spell_less_than_12_months_fup == 0,
+             is_the_study_spell := 1]
 
 #alternative for dinamic cohort (example: if a subject enter after the study start it will be included with this alternative way)--------------------
 
