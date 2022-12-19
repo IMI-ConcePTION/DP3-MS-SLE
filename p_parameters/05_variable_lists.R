0e8a07ab0f2bf55c7b18b1e22f9070dcbf2d622e
Davide <davide.messina@ars.toscana.it>
2022-12-19 18:04:28 +0100

    Added variables

--- p_parameters/05_variable_lists.R
+++ p_parameters/05_variable_lists.R
@@ -1,7 +1,9 @@
 # create few lists, each containing variables of interest for different D3s
 
 #create two list of strings:
-#OUTCOME_variables (outcomes)
-#COV_variables  (covariates)
 
+# OUTCOME_variables (outcomes)
+OUTCOME_variables <- c("MS", "SLE")
 
+# DP_variables (druf proxies)
+DP_variables <- c("DP_MS", "DP_SLE")
