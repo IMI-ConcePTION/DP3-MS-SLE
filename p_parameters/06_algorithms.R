e183f47aed515d303ac9ca06a68d38f7487b4309
Davide <davide.messina@ars.toscana.it>
2022-12-21 17:11:18 +0100

    Removed unnecessary parameters

--- p_parameters/06_algorithms.R
+++ p_parameters/06_algorithms.R
@@ -20,91 +20,91 @@ for (level1 in c("HOSP","PC","SPECIALIST","LONGTERM")) {
   }
 }
 
-#----------------------------
-# SECONDARY COMPONENTS
+# #----------------------------
+# # SECONDARY COMPONENTS
+# 
+# # SECCOMPONENTS <- c("ArterialNoTP", "ArterialTP", "VTENoTP", "VTETP", "ArterialVTENoTP", "ArterialVTETP", "CVSTNoTP", "CVSTTP")
+# SECCOMPONENTS <- NULL
+# 
+# concept_set_seccomp <- vector(mode="list")
+# rule_seccomp <- vector(mode="list")
+# distance_seccomp <- vector(mode="list")
+# direction_seccomp <- vector(mode="list")
+# 
+# for (SECCOMP in SECCOMPONENTS) {
+#   concept_set_seccomp[[SECCOMP]][['B']] <- c("TP_narrow","TP_possible")
+#   distance_seccomp[[SECCOMP]] = '10'
+#   direction_seccomp[[SECCOMP]] = "Either direction"
+#   
+#   selectionrule_direction_seccomp <- vector(mode="list")
+#   selectionrule_direction_seccomp["A before B"] <- paste0("dateA <= dateB  & dateB <= dateA + ",distance_seccomp[[SECCOMP]])
+#   selectionrule_direction_seccomp["B before A"] <- paste0("dateB <= dateA  & dateA <= dateB + ",distance_seccomp[[SECCOMP]])
+#   selectionrule_direction_seccomp["Either direction"] <- paste0('((',selectionrule_direction_seccomp["A before B"],') | (',selectionrule_direction_seccomp["B before A"],'))')
+#   
+# }
+# 
+# 
+# # ArterialNoTP
+# concept_set_seccomp[["ArterialNoTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow")
+# rule_seccomp[["ArterialNoTP"]] <- "AND NOT"
+# 
+# # ArterialTP
+# concept_set_seccomp[["ArterialTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow")
+# rule_seccomp[["ArterialTP"]] <- "AND"
+# 
+# # VTENoTP
+# concept_set_seccomp[["VTENoTP"]][['A']] <- c("VTE_narrow","VTE_possible")
+# rule_seccomp[["VTENoTP"]] <- "AND NOT"
+# 
+# # VTETP
+# concept_set_seccomp[["VTETP"]][['A']] <- c("VTE_narrow","VTE_possible")
+# rule_seccomp[["VTETP"]] <- "AND"
+# 
+# # ArterialVTENoTP
+# concept_set_seccomp[["ArterialVTENoTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow","VTE_narrow","VTE_possible")
+# rule_seccomp[["ArterialVTENoTP"]] <- "AND NOT"
+# 
+# # ArterialVTETP
+# concept_set_seccomp[["ArterialVTETP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow","VTE_narrow","VTE_possible")
+# rule_seccomp[["ArterialVTETP"]] <- "AND"
+# 
+# # CVSTNoTP
+# concept_set_seccomp[["CVSTNoTP"]][['A']] <- c("Sinusthrom_narrow","Sinusthrom_possible")
+# rule_seccomp[["CVSTNoTP"]] <- "AND NOT"
+# 
+# # CVSTTP
+# concept_set_seccomp[["CVSTTP"]][['A']] <- c("Sinusthrom_narrow","Sinusthrom_possible")
+# rule_seccomp[["CVSTTP"]] <- "AND"
+# 
+# # XXXX
+# concept_set_seccomp[["XXXX"]][['A']] <- c()
+# rule_seccomp[["XXXX"]] <- ""
 
-# SECCOMPONENTS <- c("ArterialNoTP", "ArterialTP", "VTENoTP", "VTETP", "ArterialVTENoTP", "ArterialVTETP", "CVSTNoTP", "CVSTTP")
-SECCOMPONENTS <- NULL
-
-concept_set_seccomp <- vector(mode="list")
-rule_seccomp <- vector(mode="list")
-distance_seccomp <- vector(mode="list")
-direction_seccomp <- vector(mode="list")
-
-for (SECCOMP in SECCOMPONENTS) {
-  concept_set_seccomp[[SECCOMP]][['B']] <- c("TP_narrow","TP_possible")
-  distance_seccomp[[SECCOMP]] = '10'
-  direction_seccomp[[SECCOMP]] = "Either direction"
-  
-  selectionrule_direction_seccomp <- vector(mode="list")
-  selectionrule_direction_seccomp["A before B"] <- paste0("dateA <= dateB  & dateB <= dateA + ",distance_seccomp[[SECCOMP]])
-  selectionrule_direction_seccomp["B before A"] <- paste0("dateB <= dateA  & dateA <= dateB + ",distance_seccomp[[SECCOMP]])
-  selectionrule_direction_seccomp["Either direction"] <- paste0('((',selectionrule_direction_seccomp["A before B"],') | (',selectionrule_direction_seccomp["B before A"],'))')
-  
-}
-
-
-# ArterialNoTP
-concept_set_seccomp[["ArterialNoTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow")
-rule_seccomp[["ArterialNoTP"]] <- "AND NOT"
-
-# ArterialTP
-concept_set_seccomp[["ArterialTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow")
-rule_seccomp[["ArterialTP"]] <- "AND"
-
-# VTENoTP
-concept_set_seccomp[["VTENoTP"]][['A']] <- c("VTE_narrow","VTE_possible")
-rule_seccomp[["VTENoTP"]] <- "AND NOT"
-
-# VTETP
-concept_set_seccomp[["VTETP"]][['A']] <- c("VTE_narrow","VTE_possible")
-rule_seccomp[["VTETP"]] <- "AND"
-
-# ArterialVTENoTP
-concept_set_seccomp[["ArterialVTENoTP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow","VTE_narrow","VTE_possible")
-rule_seccomp[["ArterialVTENoTP"]] <- "AND NOT"
-
-# ArterialVTETP
-concept_set_seccomp[["ArterialVTETP"]][['A']] <- c("CAD_narrow","Ischstroke_narrow","VTE_narrow","VTE_possible")
-rule_seccomp[["ArterialVTETP"]] <- "AND"
-
-# CVSTNoTP
-concept_set_seccomp[["CVSTNoTP"]][['A']] <- c("Sinusthrom_narrow","Sinusthrom_possible")
-rule_seccomp[["CVSTNoTP"]] <- "AND NOT"
-
-# CVSTTP
-concept_set_seccomp[["CVSTTP"]][['A']] <- c("Sinusthrom_narrow","Sinusthrom_possible")
-rule_seccomp[["CVSTTP"]] <- "AND"
-
-# XXXX
-concept_set_seccomp[["XXXX"]][['A']] <- c()
-rule_seccomp[["XXXX"]] <- ""
-
-# DATASOURCE-SPECIFIC ALGORITHMS
-
-datasources_with_specific_algorithms <- c('GePaRD')
-
-exclude_meanings_from_OUTCOME <- vector(mode="list")
-OUTCOME_events<-c('ABS','ADEM','GENCONV','MENINGOENC','ARD','ANAPHYL','MISCC')
-# HH HA NH NA NE NN NO NS NU A G V Z
-for (OUTCOME in OUTCOME_events){
-  exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]]=c("HA", "NA", "NE", "NN", "NO", "NS", "NU", "A", "G", "V", "Z")
-}
-for (OUTCOME in c('ABS','ADEM','GENCONV','MENINGOENC','ARD','ANAPHYL','MISCC')) {
-        exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]]=c(exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]],"G")
-}
-
-# create the rule that eliminates the meanings that are not appropriate for each outcome
-selection_meanings_from_OUTCOME <- vector(mode="list")
-if (thisdatasource %in% datasources_with_specific_algorithms){ 
-  for (OUTCOME in OUTCOME_events){
-    select <- "!is.na(person_id) "
-    for (meaningevent in exclude_meanings_from_OUTCOME[[thisdatasource]][[OUTCOME]]){
-      select <- paste0(select," & meaning_of_event!= '",meaningevent,"'")
-    }
-    selection_meanings_from_OUTCOME[[thisdatasource]][[OUTCOME]] <- select
-  }
-}
+# # DATASOURCE-SPECIFIC ALGORITHMS
+# 
+# datasources_with_specific_algorithms <- c('GePaRD')
+# 
+# exclude_meanings_from_OUTCOME <- vector(mode="list")
+# OUTCOME_events<-c('ABS','ADEM','GENCONV','MENINGOENC','ARD','ANAPHYL','MISCC')
+# # HH HA NH NA NE NN NO NS NU A G V Z
+# for (OUTCOME in OUTCOME_events){
+#   exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]]=c("HA", "NA", "NE", "NN", "NO", "NS", "NU", "A", "G", "V", "Z")
+# }
+# for (OUTCOME in c('ABS','ADEM','GENCONV','MENINGOENC','ARD','ANAPHYL','MISCC')) {
+#         exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]]=c(exclude_meanings_from_OUTCOME[["GePaRD"]][[OUTCOME]],"G")
+# }
+# 
+# # create the rule that eliminates the meanings that are not appropriate for each outcome
+# selection_meanings_from_OUTCOME <- vector(mode="list")
+# if (thisdatasource %in% datasources_with_specific_algorithms){ 
+#   for (OUTCOME in OUTCOME_events){
+#     select <- "!is.na(person_id) "
+#     for (meaningevent in exclude_meanings_from_OUTCOME[[thisdatasource]][[OUTCOME]]){
+#       select <- paste0(select," & meaning_of_event!= '",meaningevent,"'")
+#     }
+#     selection_meanings_from_OUTCOME[[thisdatasource]][[OUTCOME]] <- select
+#   }
+# }
 
 # concept sets specific for datasources
 
@@ -116,7 +116,7 @@ if (thisdatasource %in% datasources_with_specific_algorithms){
 #-------------------------------------
 # set concept sets
 
-# concept_set_codes_our_study <- concept_set_codes_our_study_pre
+concept_set_codes_our_study <- concept_set_codes_our_study_pre
 # concept_set_codes_our_study_excl <- concept_set_codes_our_study_pre_excl
 
 # # augment ICPC codes
@@ -155,14 +155,14 @@ for (conceptset in concept_sets_of_our_study){
 # fix for ICD9CM
 for (conceptset in concept_sets_of_our_study){
   if (concept_set_domains[[conceptset]] == "Diagnosis"){
-    concept_set_codes_our_study[[conceptset]][["ICD9CM"]] <- concept_set_codes_our_study[[conceptset]][["ICD9"]]
+    concept_set_codes_our_study[[conceptset]][["ICD9"]] <- concept_set_codes_our_study[[conceptset]][["ICD9CM"]]
   }
 }
 
 
-save(concept_set_codes_our_study,file=paste0(direxp,"concept_set_codes_our_study.RData"))
+save(concept_set_codes_our_study,file = paste0(direxp, "concept_set_codes_our_study.RData"))
 # save(concept_set_codes_our_study_excl,file=paste0(direxp,"concept_set_codes_our_study_excl.RData"))
-save(concept_set_codes_our_study,file=paste0(dirsmallcountsremoved,"concept_set_codes_our_study.RData"))
+save(concept_set_codes_our_study,file = paste0(dirsmallcountsremoved, "concept_set_codes_our_study.RData"))
 # save(concept_set_codes_our_study_excl,file=paste0(dirsmallcountsremoved,"concept_set_codes_our_study_excl.RData"))
 
-rm(conceptset, datasources_with_specific_algorithms, level1, meaning, OUTCOME, OUTCOME_events, SECCOMP)
+rm(conceptset, level1, meaning, concept_set_codes_our_study_pre)
