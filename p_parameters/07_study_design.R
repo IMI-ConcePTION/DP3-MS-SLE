6e9ad81a7e8f626ecc2340b5161a7e0402809dd3
Davide <davide.messina@ars.toscana.it>
2022-12-21 17:10:43 +0100

    cleaned study design

--- p_parameters/07_study_design.R
+++ p_parameters/07_study_design.R
@@ -4,20 +4,10 @@
 
 #study_years_datasource
 
-firstjan2021 <- ymd(20210101)
+study_start <- ymd(20050101)
 
-study_start <- ymd(20200101)
-start_lookback <- ymd(20190101)
-
-study_end <- min(instance_creation, recommended_end_date, na.rm = T)
+study_end <- min(instance_creation, recommended_end_date, ymd(20191231), na.rm = T)
 rm(recommended_end_date)
-
-study_years <- c("2020","2021")
-
-
-firstYearComponentAnalysis <- "2019"
-secondYearComponentAnalysis <- "2020"
-
 
 #----------------------------
 # admissible gap between observation periods (DAP-specific)
@@ -29,6 +19,7 @@ admissible_gap_obs_periods[['BIPS']] <- 30
 gap_days <- ifelse(thisdatasource %not in% names(admissible_gap_obs_periods),
                1, admissible_gap_obs_periods[[thisdatasource]])
 
+rm(admissible_gap_obs_periods)
 
 # define number of days a spells should not be shorter
 min_spell_lenght <- 365
