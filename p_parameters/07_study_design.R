4bd123e6e7ef81e44b425f0de6e7b5673417cf4e
Davide <davide.messina@ars.toscana.it>
2022-12-23 17:04:07 +0100

    Added parameter recommended_start_date

--- p_parameters/07_study_design.R
+++ p_parameters/07_study_design.R
@@ -18,8 +18,15 @@ admissible_gap_obs_periods[['BIPS']] <- 30
 
 gap_days <- ifelse(thisdatasource %not in% names(admissible_gap_obs_periods),
                1, admissible_gap_obs_periods[[thisdatasource]])
-
 rm(admissible_gap_obs_periods)
 
 # define number of days a spells should not be shorter
 min_spell_lenght <- 365
+
+recommended_start_date_vect <- vector(mode = "list")
+recommended_start_date_vect[['ARS']] <- ymd(20030101)
+recommended_start_date_vect[['TEST']] <- ymd(20030101)
+
+recommended_start_date <- fifelse(thisdatasource %not in% names(recommended_start_date_vect),
+                   ymd(19000101), recommended_start_date_vect[[thisdatasource]])
+rm(recommended_start_date_vect)
