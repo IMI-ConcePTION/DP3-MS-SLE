bf2673ea006c99ff667d971f9203652eb81a4608
Davide <davide.messina@ars.toscana.it>
2023-01-10 20:48:51 +0100

    Select last spell except EFEMERIS and THL

--- p_steps/01_T2_40_clean_spells.R
+++ p_steps/01_T2_40_clean_spells.R
@@ -7,7 +7,7 @@
 smart_load("D3_PERSONS", dirtemp)
 smart_load("D3_output_spells_category", dirtemp)
 
-# COmbine persons and spells, then select only the column we need and create new ones
+# Combine persons and spells, then select only the column we need and create new ones
 person_spell <- merge(D3_output_spells_category, D3_PERSONS, all.x = T, by = "person_id")
 person_spell <- person_spell[, .(person_id, birth_date, death_date, entry_spell_category_crude = entry_spell_category,
                                  exit_spell_category_crude = exit_spell_category, op_meaning, num_spell)]
@@ -16,13 +16,14 @@ person_spell <- person_spell[, .(person_id, birth_date, death_date, entry_spell_
 person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_spell_category_crude - 60 |
                                                              birth_date > instance_creation,
                                                            entry_spell_category_crude, birth_date)]
+
 # Censor spells which start before the recommended start date
 person_spell[, entry_spell_category := pmax(entry_spell_category, recommended_start_date, na.rm = T)]
 
 # Censor spells which ends after the death date or study_end
 person_spell[, exit_spell_category := pmin(exit_spell_category_crude, death_date, study_end, na.rm = T)]
 
-# Create variable which syas if the start/end of spell has been changed
+# Create variable which says if the start/end of spell has been changed
 person_spell[, op_start_date_cleaned := data.table::fifelse(entry_spell_category != entry_spell_category_crude, 0, 1)]
 person_spell[, op_end_date_cleaned := data.table::fifelse(exit_spell_category != exit_spell_category_crude, 0, 1)]
 
@@ -38,8 +39,15 @@ person_spell[, spell_less_than_12_months_fup := data.table::fifelse(
   correct_difftime(exit_spell_category, entry_spell_category) < min_spell_lenght &
     thisdatasource %not in% c("EFEMERIS", "THL"), 1, 0)]
 
-
 # add a criteria that identify the specific spell of interest
 person_spell[, is_the_study_spell := data.table::fifelse(starts_after_ending == 0 & no_overlap_study_period == 0 & spell_less_than_12_months_fup == 0, 1, 0)]
+
+# Remove spells which are not last
+person_spell[is_the_study_spell == 1, max_exit_spell_category := max(exit_spell_category), by = person_id]
+person_spell[exit_spell_category != max_exit_spell_category &
+               thisdatasource %not in% c("EFEMERIS", "THL"), is_the_study_spell := 0]
+person_spell[, c("max_exit_spell_category") := NULL]
+
+person_spell[is.na(is_the_study_spell), is_the_study_spell := 0]
 
 smart_save(person_spell, dirtemp, override_name = "D3_clean_spells")
