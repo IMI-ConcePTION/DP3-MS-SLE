717d6fcfa37e5e9c296246ebdcd79493aa08bed0
Davide Messina <davide.messina@ars.toscana.it>
2023-01-13 19:43:23 +0100

    Continuing development

--- p_steps/03_T2_30_create_algorithms_MS.R
+++ p_steps/03_T2_30_create_algorithms_MS.R
@@ -4,6 +4,7 @@
 
 #TODO remove when activating SLE
 OUTCOME_variables <- "MS"
+s <- c(999, 8, 5, 3, 2, 1)
 
 for (outcome in OUTCOME_variables) {
   
@@ -24,4 +25,29 @@ for (outcome in OUTCOME_variables) {
   setcolorder(D3_algorithms_MS, c("person_id", "date", "algorithm"))
   
   smart_save(D3_algorithms_MS, dirtemp)
+  
+  # Select algorithms columns and spells
+  algorithms_dates_spells <- components[, c("person_id", "cohort_entry_date", "cohort_exit_date", "entry_spell_category", algo_cols), with = F]
+
+  # Keep only persons with at least one algorithm positive
+  algorithms_dates_spells <- algorithms_dates_spells[rowSums(!is.na(algorithms_dates_spells[, ..algo_cols])) > 0, ]
+  
+  # Keep only women who exit the study at 31st december 2019
+  algorithms_dates_spells <- algorithms_dates_spells[cohort_exit_date == ymd(20191231), ]
+  
+  # Calculate length of spell
+  algorithms_dates_spells[, length_spell := age_fast(entry_spell_category, cohort_exit_date)]
+  
+  # Find if spells are longer than 5 and 10 years
+  algorithms_dates_spells[, at_least_5_years_of_lookback_at_20191231 := as.integer(length_spell >= 5)]
+  algorithms_dates_spells[, at_least_10_years_of_lookback_at_20191231 := as.integer(length_spell >= 10)]
 }
+
+
+
+
+
+
+
+
+
