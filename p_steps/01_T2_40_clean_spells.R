940d8287db421f690751f8dcd38f6177f5a48e07
Davide <davide.messina@ars.toscana.it>
2023-01-19 09:55:14 +0100

    Clean spells now consider case when spell is excluded because of age

--- p_steps/01_T2_40_clean_spells.R
+++ p_steps/01_T2_40_clean_spells.R
@@ -12,6 +12,7 @@ person_spell <- merge(D3_output_spells_category, D3_PERSONS, all.x = T, by = "pe
 person_spell <- person_spell[, .(person_id, birth_date, death_date, entry_spell_category_crude = entry_spell_category,
                                  exit_spell_category_crude = exit_spell_category, op_meaning, num_spell)]
 
+# TODO censor
 # If the spell start within 60 days from the birth the spell start becomes the birth date
 person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_spell_category_crude - 60 |
                                                              birth_date > instance_creation,
@@ -20,19 +21,26 @@ person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_sp
 # Censor spells which start before the recommended start date
 person_spell[, entry_spell_category := pmax(entry_spell_category, recommended_start_date, na.rm = T)]
 
-# Censor spells which ends after the death date or study_end
+# Censor spells which ends after the death date, study_end or 50th birthday
 person_spell[, exit_spell_category := pmin(exit_spell_category_crude, death_date, study_end, na.rm = T)]
+
+# find spells that end before they start (using original start/end)
+person_spell[, starts_after_ending := data.table::fifelse(entry_spell_category_crude <= exit_spell_category_crude, 0, 1)]
+
+# Censor spells which ends after the 50th birthday
+person_spell[, exit_spell_category := pmin(exit_spell_category, birth_date + years(50) - 1, na.rm = T)]
 
 # Create variable which says if the start/end of spell has been changed
 person_spell[, op_start_date_cleaned := data.table::fifelse(entry_spell_category != entry_spell_category_crude, 0, 1)]
 person_spell[, op_end_date_cleaned := data.table::fifelse(exit_spell_category != exit_spell_category_crude, 0, 1)]
 
-# find spells that end before they start (using original start/end)
-person_spell[, starts_after_ending := data.table::fifelse(entry_spell_category_crude < exit_spell_category_crude, 0, 1)]
-
 # find spells that do not overlap the study period (using original start/end)
 person_spell[, no_overlap_study_period := data.table::fifelse(
-  entry_spell_category_crude > study_end | exit_spell_category_crude < study_start, 1, 0)]
+  entry_spell_category > study_end | exit_spell_category < study_start, 1, 0)]
+
+# Find if person is too old or young when spell start/end
+person_spell[, too_old_at_start_spell := data.table::fifelse(entry_spell_category > exit_spell_category, 1, 0)]
+person_spell[, too_young_at_exit_spell := data.table::fifelse(exit_spell_category < birth_date + years(15), 1, 0)]
 
 # find spells that are shorter than x days (using cleaned start/end)
 person_spell[, spell_less_than_12_months_fup := data.table::fifelse(
@@ -40,7 +48,7 @@ person_spell[, spell_less_than_12_months_fup := data.table::fifelse(
     thisdatasource %not in% c("EFEMERIS", "THL"), 1, 0)]
 
 # add a criteria that identify the specific spell of interest
-person_spell[, is_the_study_spell := data.table::fifelse(starts_after_ending == 0 & no_overlap_study_period == 0 & spell_less_than_12_months_fup == 0, 1, 0)]
+person_spell[, is_the_study_spell := data.table::fifelse(starts_after_ending == 0 & no_overlap_study_period == 0 & too_old_at_start_spell == 0 & too_young_at_exit_spell == 0 & spell_less_than_12_months_fup == 0, 1, 0)]
 
 # Remove spells which are not last
 person_spell[is_the_study_spell == 1, max_exit_spell_category := max(exit_spell_category), by = person_id]
