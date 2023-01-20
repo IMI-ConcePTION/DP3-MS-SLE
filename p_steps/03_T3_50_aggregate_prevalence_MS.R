562c318760a9e04a32dad20b731255d2c40d9c0b
Davide <davide.messina@ars.toscana.it>
2023-01-20 09:52:05 +0100

    Aggregate the prevalence

---
+++ p_steps/03_T3_50_aggregate_prevalence_MS.R
@@ -0,0 +1,31 @@
+# Create D4_prevalence_aggregated_all_MS
+# input: D3_study_population_SAP1, conceptset
+# output: D3_clean_spells
+
+#TODO remove when activating SLE
+OUTCOME_variables <- "MS"
+
+for (outcome in OUTCOME_variables) {
+  
+  # Load period prevalence
+  D4_prevalence_period <- smart_load(paste("D4_prevalence_period", outcome, sep = "_"), diroutput, return = T)
+  
+  # Load persontime prevalence
+  D4_prevalence_persontime <- smart_load(paste("D4_prevalence_persontime", outcome, sep = "_"), diroutput, return = T)
+  D4_prevalence_persontime[, Persontime := NULL]
+  
+  # Load average point prevalence
+  D4_prevalence_average_point <- smart_load(paste("D4_prevalence_average_point", outcome, sep = "_"), diroutput, return = T)
+  D4_prevalence_average_point <- D4_prevalence_average_point[, lapply(.SD, sum, na.rm = T),
+                                                             by = c("type_of_prevalence", "ageband",
+                                                                    "timeframe", "algorithm"),
+                                                             .SDcols = c(paste("numerator", seq_len(60), sep="_"),
+                                                                         paste("denominator", seq_len(60), sep="_"))]
+  
+  D4_prevalence_aggregated_all <- rbindlist(list(D4_prevalence_period, D4_prevalence_persontime, D4_prevalence_average_point),
+                                            use.names = T, fill = T)
+  
+  
+  smart_save(D4_prevalence_aggregated_all, direxp, override_name = paste("D4_prevalence_aggregated_all",
+                                                                            outcome, sep = "_"))
+}
