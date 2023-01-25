9fa38d7c232a7aa7dd5077434d2396bbd296f2a0
Davide <davide.messina@ars.toscana.it>
2023-01-25 17:00:41 +0100

    Changing how algorithm lookback is created

--- p_steps/03_T2_30_create_algorithms_MS.R
+++ p_steps/03_T2_30_create_algorithms_MS.R
@@ -38,6 +38,102 @@ for (outcome in OUTCOME_variables) {
     next
   }
   
+  # Load components
+  components_lookback <- smart_load(paste("D3_components_multiple_lookback", outcome, sep = "_"), dirtemp, return = T)
+  
+  # Recode all 1 to the length of lokkback
+  components_lookback[at_least_5_years_of_lookback_at_20191231 == 1, at_least_5_years_of_lookback_at_20191231 := 5]
+  components_lookback[at_least_10_years_of_lookback_at_20191231 == 1, at_least_10_years_of_lookback_at_20191231 := 10]
+  
+  # Select algorithms columns
+  algo_cols <- colnames(components_lookback)[grepl("M[0-9]_([0-9]|all)_date", colnames(components_lookback))]
+  
+  # Lookback to a single column
+  components_lookback <- data.table::melt(components_lookback,
+                                          id.vars = c("person_id", "cohort_entry_date", "cohort_exit_date", algo_cols),
+                                          measure.vars = c("at_least_5_years_of_lookback_at_20191231",
+                                                           "at_least_10_years_of_lookback_at_20191231"),
+                                          variable.name = "original_var", variable.factor = F,
+                                          value.name = "years_of_lookback_at_20191231")
+  components_lookback[, original_var := NULL]
+  
+  # Remove all at_least_10_years_of_lookback_at_20191231 == 0 that where there before
+  components_lookback <- components_lookback[years_of_lookback_at_20191231 != 0, ]
+  
+  # Algorithm to a single column
+  components_lookback <- data.table::melt(components_lookback, id.vars = c("person_id", "years_of_lookback_at_20191231"),
+                                measure.vars = algo_cols,
+                                variable.name = "algorithm", variable.factor = F,
+                                value.name = "date_algo")
+  
+  # Clean the algorithm columns and add the years off lookback
+  components_lookback[, algorithm := gsub("_date", "", algorithm)]
+  components_lookback[, algorithm := paste(algorithm, years_of_lookback_at_20191231, sep = "-")]
+  algo_cols <- paste(gsub("_date", "", algo_cols), unique(components_lookback[, years_of_lookback_at_20191231]), sep = "-")
+  components_lookback[, years_of_lookback_at_20191231 := NULL]
+  
+  # Get birth date from study_population
+  smart_load("D3_study_population_SAP1", dirtemp)
+  D3_study_population_SAP1 <- D3_study_population_SAP1[, .(person_id, start_observation_period = entry_spell_category,
+                                                           cohort_entry_date, cohort_exit_date, birth_date)]
+  
+  period_prevalence <- lapply(s, function(x){
+    int_x <- if (x == "all") 99 else as.integer(x)
+    D3_study_population_SAP1[, censored_entry_date := study_end - years(int_x) + 1]
+    D3_study_population_SAP1[, cohort_exit_date_2 := study_end]
+    components_lookback <- components_lookback[!is.na(date_algo)]
+    temp <- CountPrevalence(D3_study_population_SAP1,
+                    components_lookback[grepl(paste0("M[0-9]_", x,"-"), algorithm), ], "person_id",
+                    Start_date = "cohort_entry_date",
+                    End_date = "cohort_exit_date", Birth_date = "birth_date",
+                    Name_condition = "algorithm", Date_condition = "date_algo",
+                    Type_prevalence = "period",
+                    Periods_of_time = list(c("censored_entry_date", "cohort_exit_date_2")),
+                    Start_study_time = recommended_start_date, End_study_time = study_end,
+                    Conditions = unique(components_lookback[grepl(paste0("M[0-9]_", x,"-"), algorithm), algorithm]),
+                    include_remaning_ages = F,
+                    Age_bands = ageband_definition, Aggregate = F)
+    temp <- temp[, timeframe := NULL]
+    
+  })
+  
+  MergedDT = Reduce(function(...) merge(..., all = T, by = c("person_id", "cohort_entry_date", "cohort_exit_date",
+                                                             "in_population", "")), period_prevalence)
+  period_prevalence <- rbindlist(period_prevalence, fill = T)
+  
+  # Remove period not in population
+  period_prevalence <- period_prevalence[in_population == 1,][, in_population := NULL]
+  
+  # Algorithm to a single column
+  algo_cols <- colnames(period_prevalence)[grepl(paste(algo_cols, collapse = "|"), colnames(period_prevalence))]
+  period_prevalence <- period_prevalence[, (algo_cols) := lapply(.SD, as.integer), .SDcols = algo_cols]
+  period_prevalence <- data.table::melt(period_prevalence,
+                                        id.vars = c("person_id", "cohort_entry_date", "cohort_exit_date",
+                                                    "timeframe", "Ageband"),
+                                        measure.vars = algo_cols,
+                                        variable.name = "algorithm", variable.factor = F,
+                                        value.name = "flag_algo")
+  
+  period_prevalence <- period_prevalence[, c("algorithm", "years_of_lookback_at_20191231") := tstrsplit(algorithm, "-")]
+
+  # Rewiden years_of_lookback_at_20191231
+  test <- data.table::dcast(period_prevalence,
+                            person_id + cohort_entry_date + cohort_exit_date + at_least_5_years_of_lookback_at_20191231 +
+                              at_least_10_years_of_lookback_at_20191231 ~ length_lookback,
+                            drop = T, value.var = algo_cols)
+  
+  
+  components_lookback <- data.table::melt(components_lookback,
+                                          id.vars = c("person_id", "cohort_entry_date", "cohort_exit_date", algo_cols),
+                                          measure.vars = c("at_least_5_years_of_lookback_at_20191231",
+                                                           "at_least_10_years_of_lookback_at_20191231"),
+                                          variable.name = "original_var", variable.factor = F,
+                                          value.name = "years_of_lookback_at_20191231")
+  
+  
+  
+  
+  
   # Select algorithms columns and spells
   algorithms_dates_spells <- components[, c("person_id", "cohort_entry_date", "cohort_exit_date", "entry_spell_category", algo_cols), with = F]
   
