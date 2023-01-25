69c6c51389017d50a3b26b843e360709670e5725
Davide <davide.messina@ars.toscana.it>
2023-01-25 16:59:16 +0100

    Cleaning creation of D3 study pop

--- p_steps/03_T2_10_create_study_population_baseline_variables.R
+++ p_steps/03_T2_10_create_study_population_baseline_variables.R
@@ -6,19 +6,14 @@
 # Load person_id in study_populations
 smart_load("D4_study_population_SAP1", diroutput)
 
-# Load persons information
-smart_load("D3_PERSONS", dirtemp)
-D3_PERSONS <- D3_PERSONS[, .(person_id, birth_date)]
-
 # Import the spells and clean
 smart_load("D3_clean_spells", dirtemp)
-D3_clean_spells <- D3_clean_spells[, .(person_id, entry_spell_category, exit_spell_category,
+D3_clean_spells <- D3_clean_spells[, .(person_id, entry_spell_category, exit_spell_category, birth_date,
                                        is_the_study_spell)]
 D3_clean_spells <- D3_clean_spells[is_the_study_spell == 1, ][, is_the_study_spell := NULL]
 
 # Merge all datasets
-D3_study_population_SAP1 <- merge(D4_study_population_SAP1, D3_PERSONS, all.x = T, by = "person_id")
-D3_study_population_SAP1 <- merge(D3_study_population_SAP1, D3_clean_spells, all.x = T, by = "person_id")
+D3_study_population_SAP1 <- merge(D4_study_population_SAP1, D3_clean_spells, all.x = T, by = "person_id")
 
 # Calculate cohort entry and exit date (censor for age and study_start)
 D3_study_population_SAP1[, cohort_entry_date := pmax(entry_spell_category, study_start, birth_date + floor(15 * 365.25))]
@@ -28,7 +23,7 @@ D3_study_population_SAP1 <- D3_study_population_SAP1[, exit_spell_category := NU
 # Calculate age at start of spells
 D3_study_population_SAP1[, age_at_entry_spell_category := age_fast(birth_date, cohort_entry_date)]
 
-# Calculate the lenght of the spell in years and cut it in categories
+# Calculate the length of the spell in years
 D3_study_population_SAP1[, for_n_years_in_study := age_fast(cohort_entry_date, cohort_exit_date + 1)]
 
 # Save the dataset
