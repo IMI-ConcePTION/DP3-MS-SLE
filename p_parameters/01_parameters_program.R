b83e7ddb5ce19478689ad45abad54f913aedc567
Davide <davide.messina@ars.toscana.it>
2023-02-01 15:43:56 +0100

    Added packages dplyr and purrr

--- p_parameters/01_parameters_program.R
+++ p_parameters/01_parameters_program.R
@@ -38,7 +38,7 @@ read_library <- function(...) {
   invisible(lapply(x, library, character.only = TRUE))
 }
 
-list.of.packages <- c("lubridate", "stringr", "readr", "data.table", "readxl", "qs")
+list.of.packages <- c("lubridate", "stringr", "readr", "data.table", "readxl", "qs", "dplyr", "purrr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)) install.packages(new.packages)
 invisible(lapply(list.of.packages, require, character.only = T))
