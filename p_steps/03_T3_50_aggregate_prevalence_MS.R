e603f04cd6aa1c61d73652bcab9792994c3f439f
Davide <davide.messina@ars.toscana.it>
2023-02-01 15:45:16 +0100

    Keep only ageband all for single years and MS1 -> M1

--- p_steps/03_T3_50_aggregate_prevalence_MS.R
+++ p_steps/03_T3_50_aggregate_prevalence_MS.R
@@ -7,10 +7,12 @@ for (outcome in OUTCOME_variables) {
   
   # Load period prevalence
   D4_prevalence_period <- smart_load(paste("D4_prevalence_period", outcome, sep = "_"), diroutput, return = T)
+  D4_prevalence_period <- D4_prevalence_period[grepl("-", timeframe) | ageband == "all", ]
   
   # Load persontime prevalence
   D4_prevalence_persontime <- smart_load(paste("D4_prevalence_persontime", outcome, sep = "_"), diroutput, return = T)
   D4_prevalence_persontime[, Persontime := NULL]
+  D4_prevalence_persontime <- D4_prevalence_persontime[grepl("-", timeframe) | ageband == "all", ]
   
   # Load average point prevalence
   D4_prevalence_average_point <- smart_load(paste("D4_prevalence_average_point", outcome, sep = "_"), diroutput, return = T)
@@ -23,7 +25,7 @@ for (outcome in OUTCOME_variables) {
   D4_prevalence_aggregated_all <- rbindlist(list(D4_prevalence_period, D4_prevalence_persontime, D4_prevalence_average_point),
                                             use.names = T, fill = T)
   
-  D4_prevalence_aggregated_all <- D4_prevalence_aggregated_all[, datasource := thisdatasource]
+  D4_prevalence_aggregated_all[, datasource := thisdatasource][, algorithm := gsub("^MS", "M", algorithm)]
   
   smart_save(D4_prevalence_aggregated_all, direxp,
              override_name = paste("D4_prevalence_aggregated_all", outcome, sep = "_"),
