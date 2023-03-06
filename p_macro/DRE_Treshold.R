4df2932f23c4d9277804544fcb0efabcfbc3322c
Davide <davide.messina@ars.toscana.it>
2023-03-06 11:21:15 +0100

    Reworked DRE_threshold

--- p_macro/DRE_Treshold.R
+++ p_macro/DRE_Treshold.R
@@ -1,6 +1,7 @@
-DRE_Treshold <- function(Inputfolder, Outputfolder, Varlist = NULL, Delimiter = "auto", NAlist = NULL, FileContains = NULL){
+DRE_Treshold <- function(Inputfolder, Outputfolder, Varlist = NULL, Delimiter = ",", NAlist = NULL, FileContains = NULL){
   
   if(is.null(FileContains)) FileContains <- "."
+  Varlist <- sapply(unique(names(Varlist)), function(x) Varlist[[x]])
   
   Inputfolder <- gsub("/$", "", Inputfolder)
   Outputfolder <- gsub("/$", "", Outputfolder)
@@ -18,9 +19,12 @@ DRE_Treshold <- function(Inputfolder, Outputfolder, Varlist = NULL, Delimiter = 
       
       for(x in 1:length(Varlist_in_file)) {
         Var_in_file <- names(Varlist_in_file[x])
+        File[, (Var_in_file) := as.character(get(Var_in_file))]
         
-        if(any(colnames(File) %in% NAlist)) File[get(Var_in_file) < Varlist_in_file[[x]], which(colnames(File) %in% NAlist)] <- NA
-        File[get(Var_in_file) < Varlist_in_file[[x]] & get(Var_in_file) > 0 , (Var_in_file) := paste0("< ", Varlist_in_file[x])] 
+        if(any(colnames(File) %in% NAlist)) File[as.integer(get(Var_in_file)) < Varlist_in_file[[x]],
+                                                 which(colnames(File) %in% NAlist)] <- NA
+        File[as.integer(get(Var_in_file)) < Varlist_in_file[[x]] & as.integer(get(Var_in_file)) > 0 ,
+             (Var_in_file) := paste0("< ", Varlist_in_file[x])] 
       }
     }
     
