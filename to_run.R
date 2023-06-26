2be5a4fa934dd88b36b57d09e079f7e058791a60
Davide <davide.messina@ars.toscana.it>
2023-06-26 18:10:25 +0200

    v1.0.5 to 2.0.0

--- to_run.R
+++ to_run.R
@@ -4,7 +4,7 @@
 # authors: Rosa Gini, Davide Messina, Marie Beslay
 # based on previous scripts
 
-# v1.0.5 - 26 June 2023
+# v2.0.0 - 26 June 2023
 # Added Cube function to aggregate data in dimensions
 # Removed final datasets
 # Reworked the masking of datasets
