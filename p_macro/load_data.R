8a840fa3dced4e1c2f9ce0eaa44a8ec239105b35
Davide <davide.messina@ars.toscana.it>
2023-10-26 12:20:42 +0200

    moved function to p_macro

---
+++ p_macro/load_data.R
@@ -0,0 +1,8 @@
+# Load index and find if each entry have a respective codebook
+data <- readxl::read_excel(index_loc, sheet = "index")
+
+missing_codebooks <- setdiff(data$dataset_name, tools::file_path_sans_ext(list.files(codebooks_loc)))
+if (length(missing_codebooks) > 0) {
+  message(paste("Missing codebooks for:", paste(missing_codebooks, collapse = ", ")))
+}
+rm(data, missing_codebooks)
