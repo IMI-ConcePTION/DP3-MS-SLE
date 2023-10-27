44c3c8824477e1e93b139ccdb197185925d603f7
Davide <davide.messina@ars.toscana.it>
2023-10-27 12:25:30 +0200

    to_run v2.3.6 (updated)

--- to_run.R
+++ to_run.R
@@ -7,6 +7,8 @@
 # v2.3.6 - 26 October 2023
 # Modified D5_N_women_and_ranges with statistics for DAPs with only pregnancies
 # Fixed bug in spell_length calculation
+# Added Hmisc package
+# Removed duplicated rows for N_women_and_ranges in case of DAPs with only pregnancies
 
 # v2.3.5 - 23 October 2023
 # Readded explicit levels used by Cube in the final dataset
