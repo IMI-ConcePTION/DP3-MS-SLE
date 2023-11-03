5a6d7431fe9c5907ab668b3d6f2998c2298d375f
Davide <davide.messina@ars.toscana.it>
2023-11-03 15:30:44 +0100

    update to_run with simple instructions

--- to_run.R
+++ to_run.R
@@ -1,7 +1,8 @@
+# Create new datamodels
 source(paste0(getwd(), "/p_macro/to_run_create_diagram.R"))
 
-setwd(paste0(getwd(), "/i_website"))
-source(here::here("i_website", "R", "load_functions.R"))
-source(here::here("i_website", "R", "load_data.R"))
-
+# Transform to html
 blogdown::build_site(build_rmd = T)
+
+# Move to docs folder(publishable)
+blogdown::serve_site()
