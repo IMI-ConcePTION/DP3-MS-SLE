efadb2a3653f9558128c1079c77df9bd853dfa5c
Davide <davide.messina@ars.toscana.it>
2023-11-07 11:14:38 +0100

    Rmd to HTML only for updated Rmd

--- p_macro/generate_website.R
+++ p_macro/generate_website.R
@@ -1,3 +1,13 @@
 generate_website <- function() {
-  blogdown::build_site(build_rmd = T)
+  
+  all_changes <- gert::git_status(pathspec = "i_codebooks")
+  change_codebook <- all_changes %>%
+    dplyr::filter(basename(file) != "00_index.xlsx")
+  
+  files_in_content <- list.files(here::here("i_website", "content"), recursive = T)
+  changes <- files_in_content[sub('\\..*$', '', basename(files_in_content)) %in% sub('\\..*$', '', basename(change_codebook$file))]
+  
+  changes <- file.path("content", changes)
+  
+  blogdown::build_site(build_rmd = changes)
 }
