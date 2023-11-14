4cd48094131e11629915d7768697ca7dc34f850f
RosaMGini <rosa.m.gini@gmail.com>
2023-11-14 17:03:17 +0100

    note steps matching

---
+++ p_steps/09_T3_10_match.R
@@ -0,0 +1,6 @@
+# Match D3_DU_MS_pregnancy_cohort to D3_DU_MS_cohort
+# input: D3_DU_MS_pregnancy_cohort, D3_DU_MS_cohort
+# output: D4_DU_matched_MS-PREGNANCY-COHORT_to_MS-COHORT
+
+# from SAP 4.3.2 Cohort of matched non-pregnant women of childbearing age with MS
+
