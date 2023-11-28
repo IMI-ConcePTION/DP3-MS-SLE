368aba5e334f86a5d5bcf29704e7f6b41da52fc5
RosaMGini <rosa.m.gini@gmail.com>
2023-11-28 11:53:08 +0100

    update some codebooks

--- to_run.R
+++ to_run.R
@@ -1,3 +1,7 @@
+
+# build the local site (that can be browsed immediately from ./g_website/static/diagram_local.html)
+serve_site()
+
 # Create/update DAG
 generate_DAG()
 
@@ -5,8 +9,6 @@ generate_DAG()
 generate_Rmd()
 generate_website()
 
-# build the local site (that can be browsed immediately from ./g_website/static/diagram_local.html)
-serve_site()
 
 # commit updated DAG, index and codebooks (after the committed changes are pushed, the site will be updated by GitHub within some minutes)
 stage_website()
