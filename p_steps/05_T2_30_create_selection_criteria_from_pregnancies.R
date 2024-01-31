e3df932558d162d300d0ae9f601f531ad29872a3
Davide <davide.messina@ars.toscana.it>
2024-01-31 23:03:29 +0100

    Initial load of step 05_T2_30

---
+++ p_steps/05_T2_30_create_selection_criteria_from_pregnancies.R
@@ -0,0 +1,9 @@
+##%######################################################%##
+#                                                          #
+####   CREATE EXCLUSION CRITERIA FOR PREGNANCY COHORT   ####
+#                                                          #
+##%######################################################%##
+
+load(paste0(dirpregnancy, "D3_pregnancy_final.Rdata"))
+
+D3_pregnancy_final
