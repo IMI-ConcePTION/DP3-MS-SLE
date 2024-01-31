26f6f41c21d8a16b56bc529f42f1075266cc0f4b
Davide Messina <davide.messina@ars.toscana.it>
2024-01-31 20:01:31 +0100

    Created step 05_T2_20

---
+++ p_steps/05_T2_20_create_selection_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort.R
@@ -0,0 +1,18 @@
+##%######################################################%##
+#                                                          #
+####      CREATE EXCLUSION CRITERIA FOR MS COHORT       ####
+#                                                          #
+##%######################################################%##
+
+smart_load("D3_SAP1_MS_COHORT", dirtemp, extension = extension)
+
+# Remove persons without MS
+selection_criteria <- D3_SAP1_MS_COHORT[, never_positive_for_MS_chosen := fifelse(is.na(date_MS), 1, 0)]
+
+# Remove persons with less than 1 year of followup after diagnosis
+selection_criteria[, women_with_less_than_1_year_fup := fifelse(
+  difftime(cohort_exit_date, date_MS, units = "days") < 365, 1, 0)]
+
+smart_save(D3_sel_cri, dirtemp, override_name = "D3_DU_selection_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort",
+           extension = extension, save_copy = "csv")
+  
