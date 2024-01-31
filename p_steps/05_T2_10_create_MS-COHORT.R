3b6ac7a5597412cd7fcec7c3ad57b2d85db446c4
Davide Messina <davide.messina@ars.toscana.it>
2024-01-31 19:17:02 +0100

    created 05_T2_10

---
+++ p_steps/05_T2_10_create_MS-COHORT.R
@@ -0,0 +1,25 @@
+##%######################################################%##
+#                                                          #
+####  SELECT SAP 1 POPULATION USING CHOSEN ALGORITHMS   ####
+#                                                          #
+##%######################################################%##
+
+smart_load("D3_study_population_SAP1", dirtemp, extension = extension)
+smart_load("D3_algorithms_MS", dirtemp, extension = extension)
+
+# Keep only records realted to the chosen algorithm from SAP 1
+D3_algorithms_MS <- D3_algorithms_MS[algorithm == chosen_MS_algorithm[[thisdatasource]], ]
+
+# Keep only necessary columns
+D3_study_population_SAP1 <- D3_study_population_SAP1[, c("person_id", "entry_spell_category", "birth_date",
+                                                         "cohort_entry_date", "cohort_exit_date")]
+D3_algorithms_MS <- D3_algorithms_MS[, c("person_id", "date")]
+
+# Left join, population into algorithm to keep only persons with a positive record for algorithm
+D3_SAP1_MS_COHORT <- merge(D3_algorithms_MS, D3_study_population_SAP1, all.x = T, by = "person_id")
+
+# Change name of variables
+setnames(D3_SAP1_MS_COHORT, "date", "date_MS")
+
+# Saving the dataset
+smart_save(D3_SAP1_MS_COHORT, dirtemp, extension = extension, save_copy = "csv")
