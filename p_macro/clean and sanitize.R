aa81ab9a43322da94d30dae69345f91b79cb5bc2
Davide <davide.messina@ars.toscana.it>
2024-02-01 12:56:51 +0100

    Sanitize excel newline in input_datasets_for_the_step

--- p_macro/clean and sanitize.R
+++ p_macro/clean and sanitize.R
@@ -1,8 +1,10 @@
 sanitize_index <- function(x) {
   lookup <- c(SLUG = "slug", PROGRAM = "step_producing_this_dataset", FOLDER_VAR = "folder_where_the_datset_is_stored",
               FILE = "dataset_name", INPUT_DATA = "input_datasets_for_the_step")
+
   return(x %>%
            rename(all_of(lookup)) %>%
+           mutate(INPUT_DATA = gsub("\\r\\n", "", INPUT_DATA)) %>%
            mutate(LINK = tolower(PROGRAM),
                   PROGRAM = stringr::str_extract(PROGRAM, "^([^_]*_){2}[^_]*")) %>%
            select(PROGRAM, FOLDER_VAR, FILE, INPUT_DATA, SLUG, LINK))
