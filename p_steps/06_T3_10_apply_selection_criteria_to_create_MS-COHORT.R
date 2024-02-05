9ea4bf266192dd3b63d48d55314674b192075d85
Davide <davide.messina@ars.toscana.it>
2024-02-05 19:40:54 +0100

    completed step 06_T3_10

---
+++ p_steps/06_T3_10_apply_selection_criteria_to_create_MS-COHORT.R
@@ -0,0 +1,35 @@
+##%######################################################%##
+#                                                          #
+####            APPLY EXCLUSION CRITERIA TO             ####
+####           CREATE STUDY POPULATION FOR DU           ####
+#                                                          #
+##%######################################################%##
+
+print('FLOWCHART')
+
+# USE THE FUNCTION CREATEFLOWCHART TO SELECT THE SUBJECTS IN POPULATION
+
+# Create flowchart for adults and save D4_study_population
+smart_load("D3_DU_selection_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort", dirtemp, extension = extension)
+selection_criteria <- D3_DU_selection_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort
+
+selected_population <- CreateFlowChart(
+  dataset = selection_criteria,
+  listcriteria = c("never_positive_for_MS_chosen", "women_diagnosed_outside_childbearing_age", "women_with_less_than_1_year_fup"),
+  flowchartname = "Flowchart_exclusion_criteria")
+
+# Find if a level contains at least a value to censor
+tmp <- copy(Flowchart_exclusion_criteria)[, N := as.character(N)]
+tmp <- tmp[as.integer(N) < summary_threshold & as.integer(N) > 0, N := "<5"]
+
+# Save flowcharts
+smart_save(tmp, direxpmask,
+           override_name = "D5_DU_flowchart_exclusion_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort_masked",
+           extension = "csv")
+smart_save(Flowchart_exclusion_criteria, direxp,
+           override_name = "D5_DU_flowchart_exclusion_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort", extension = "csv")
+
+# Save population
+smart_save(selected_population[, .(person_id)], diroutput, override_name = "D4_DU_MS_COHORT", extension = extension, save_copy = "csv")
+
+
