0a79ca70b2b13883752863f9758f791d7eb54c68
Davide <davide.messina@ars.toscana.it>
2024-03-01 17:27:18 +0100

    Updated draft for template 1

--- p_steps/08_T4_20_create_descriptives_pregnancies.R
+++ p_steps/08_T4_20_create_descriptives_pregnancies.R
@@ -17,29 +17,34 @@ tmp4 %>%
                          type = list(number_of_pregnancies_in_the_study ~ 'continuous'), 
                          statistic = list(
                            number_of_pregnancies_in_the_study ~ "{sum}"
-                         ), 
-                         by = strata, include = number_of_pregnancies_in_the_study, percent = "row") %>%
-  gtsummary::modify_header(header_string) %>%
+                         ),
+                         by = strata, include = number_of_pregnancies_in_the_study, percent = "row") |>
+  gtsummary::modify_header(header_string) |>
   gtsummary::modify_footnote(gtsummary::all_stat_cols(FALSE) ~ NA)
-
-tmp4[,total :=1] %>%
-  gtsummary::tbl_summary(label = list(number_of_pregnancies_in_the_study ~ "Study population"),
-                         by = strata, include = number_of_pregnancies_in_the_study, percent = "row") %>%
-  gtsummary::modify_footnote(gtsummary::all_stat_cols(FALSE) ~ NA)
-
 
 tmp4[, total := number_of_pregnancies_in_the_study]
 tmp4[number_of_pregnancies_in_the_study > 1 , number_of_pregnancies_in_the_study := 2]
-tmp4[, strata := factor(strata, levels = c(1, 2),
-                        labels = c('**Number of pregnancies in the Pregnancy cohort N (%)**',
-                                   '**Number of pregnancies in the MS-Pregnancy cohort N (%)**'))]
+tmp4[, strata := as.factor(strata)]
 
 tmp4 %>%
-  gtsummary::tbl_summary(label = list(number_of_pregnancies_in_the_study ~ "Study population"),
-                         by = strata, include = number_of_pregnancies_in_the_study, percent = "row") %>%
+  gtsummary::tbl_custom_summary(include = number_of_pregnancies_in_the_study,
+                         by = strata,
+                         label = list(number_of_pregnancies_in_the_study ~ "Study population"),
+                         stat_fns = number_of_pregnancies_in_the_study ~ function(data, ...) {
+                           dplyr::tibble(data) |>
+                           summarise(total = sum(total))
+                           },
+                         digits = number_of_pregnancies_in_the_study ~ 0, 
+                         statistic = number_of_pregnancies_in_the_study ~ "{total}") |>
+  # remove header row
+  gtsummary::modify_table_body(
+    ~ .x |> 
+      dplyr::filter(!(variable %in% "number_of_pregnancies_in_the_study" & row_type %in% "label"))
+  ) |>
+  gtsummary::modify_header(header_string) |>
   gtsummary::modify_footnote(gtsummary::all_stat_cols(FALSE) ~ NA)
 
 
-
-
+# stat_fns = everything() ~ function(data, ...) dplyr::tibble(value = data$value),
+# statistic = everything() ~ "{value}",
 
