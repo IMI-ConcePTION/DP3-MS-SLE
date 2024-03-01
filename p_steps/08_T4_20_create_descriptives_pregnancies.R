a89d96c24232a9fbea61e49b4e898dd7a949938c
Davide Messina <davide.messina@ars.toscana.it>
2024-03-01 20:10:28 +0100

    Calculation of distance between consecutive pregnancies

--- p_steps/08_T4_20_create_descriptives_pregnancies.R
+++ p_steps/08_T4_20_create_descriptives_pregnancies.R
@@ -1,18 +1,20 @@
 smart_load("D3_DU_PREGNANCY_COHORT_variables", dirtemp, extension = extension)
 
-tmp <- D3_DU_PREGNANCY_COHORT_variables[, .(person_id, pregnancy_id, number_of_pregnancies_in_the_study,
-                                            pregnancy_with_MS)]
+preg_cohort <- D3_DU_PREGNANCY_COHORT_variables[, .(person_id, pregnancy_id, number_of_pregnancies_in_the_study,
+                                                    pregnancy_with_MS, pregnancy_start_date, pregnancy_end_date)]
 
-tmp1 <- tmp[, .(person_id, pregnancy_id, number_of_pregnancies_in_the_study, strata = 1)]
-tmp2 <- tmp[pregnancy_with_MS == 1, .(person_id, number_of_pregnancies_in_the_study, pregnancy_id, strata = 2)]
+tmp1 <- preg_cohort[, .(person_id, pregnancy_id, number_of_pregnancies_in_the_study, pregnancy_start_date,
+                        pregnancy_end_date, strata = 1)]
+tmp2 <- preg_cohort[pregnancy_with_MS == 1, .(person_id, number_of_pregnancies_in_the_study, pregnancy_id,
+                                              pregnancy_start_date, pregnancy_end_date, strata = 2)]
 
-tmp4 <- rbindlist(list(tmp1, tmp2), use.names = T)
+preg_cohort_strat <- rbindlist(list(tmp1, tmp2), use.names = T)
 
 header_string <- list(label = '',
                       stat_1 = '**Number of pregnancies in the Pregnancy cohort N (%)**',
                       stat_2 = '**Number of pregnancies in the MS-Pregnancy cohort N (%)**')
 
-tmp4 %>%
+preg_cohort_strat %>%
   gtsummary::tbl_summary(label = list(number_of_pregnancies_in_the_study ~ "N"),
                          type = list(number_of_pregnancies_in_the_study ~ 'continuous'), 
                          statistic = list(
@@ -22,20 +24,35 @@ tmp4 %>%
   gtsummary::modify_header(header_string) |>
   gtsummary::modify_footnote(gtsummary::all_stat_cols(FALSE) ~ NA)
 
-tmp4[, total := number_of_pregnancies_in_the_study]
-tmp4[number_of_pregnancies_in_the_study > 1 , number_of_pregnancies_in_the_study := 2]
-tmp4[, strata := as.factor(strata)]
 
-tmp4 %>%
+preg_cohort_strat[, strata := as.factor(strata)]
+preg_cohort_strat_mult <- preg_cohort_strat[number_of_pregnancies_in_the_study > 2, ]
+setorder(preg_cohort_strat_mult, person_id, strata, pregnancy_start_date)
+preg_cohort_strat_mult[, dist := ceiling(as.period(interval(shift(pregnancy_end_date), pregnancy_start_date)) / months(1)),
+                       by = c("person_id", "strata")]
+preg_cohort_strat_mult <- preg_cohort_strat_mult[!is.na(dist), ]
+
+
+
+
+
+
+preg_cohort_strat[, total := number_of_pregnancies_in_the_study]
+preg_cohort_strat[number_of_pregnancies_in_the_study > 1 , number_of_pregnancies_in_the_study := 2]
+preg_cohort_strat[, number_of_pregnancies_in_the_study := factor(number_of_pregnancies_in_the_study, levels = c(1, 2),
+                                                                 labels = c("From women having 1 pregnancy",
+                                                                            "From women having more than 1 pregnancy"))]
+
+preg_cohort_strat %>%
   gtsummary::tbl_custom_summary(include = number_of_pregnancies_in_the_study,
-                         by = strata,
-                         label = list(number_of_pregnancies_in_the_study ~ "Study population"),
-                         stat_fns = number_of_pregnancies_in_the_study ~ function(data, ...) {
-                           dplyr::tibble(data) |>
-                           summarise(total = sum(total))
-                           },
-                         digits = number_of_pregnancies_in_the_study ~ 0, 
-                         statistic = number_of_pregnancies_in_the_study ~ "{total}") |>
+                                by = strata,
+                                label = list(number_of_pregnancies_in_the_study ~ "Study population"),
+                                stat_fns = number_of_pregnancies_in_the_study ~ function(data, ...) {
+                                  dplyr::tibble(data) |>
+                                    summarise(total = sum(total))
+                                },
+                                digits = number_of_pregnancies_in_the_study ~ 0, 
+                                statistic = number_of_pregnancies_in_the_study ~ "{total}") |>
   # remove header row
   gtsummary::modify_table_body(
     ~ .x |> 
