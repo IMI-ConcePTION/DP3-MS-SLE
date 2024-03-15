f5dc306950afe82e2d627a7ecce2d4c4f03dfbc3
Davide <davide.messina@ars.toscana.it>
2024-03-15 15:54:03 +0100

    to_run v3.0.0-beta

--- to_run.R
+++ to_run.R
@@ -4,6 +4,10 @@
 # authors: Rosa Gini, Davide Messina, Marie Beslay
 # based on previous scripts
 
+# v3.0.0-beta - 15 March 2024
+# Implemented template 3
+# Bugfix for template 1
+
 # v3.0.0-alpha - 04 March 2024
 # Implemented until template 1 and 2
 # A few parameters to be still setup in other DAPs so test only for EFEMERIS
@@ -37,7 +41,11 @@ source(paste0(thisdir,"/p_parameters/99_saving_all_parameters.R")) #SAVING AND C
 launch_step("p_steps/05_T2_10_create_MS-COHORT.R")
 launch_step("p_steps/05_T2_20_create_selection_criteria_from_SAP1_MS_cohort_to_DU_MS_cohort.R")
 launch_step("p_steps/05_T2_30_create_selection_criteria_from_pregnancies.R")
+launch_step("p_steps/05_T2_40_CreateConceptSetDatasetsDU.R")
 launch_step("p_steps/06_T3_10_apply_selection_criteria_to_create_MS-COHORT.R")
 launch_step("p_steps/06_T3_20_apply_selection_criteria_to_pregnancies.R")
 launch_step("p_steps/07_T2_10_collect_variables_for_pregnancies.R")
 launch_step("p_steps/08_T4_20_create_descriptives_pregnancies.R")
+launch_step("p_steps/10_T3_20_create_prevalence_of_exclusive_use_MSmeds_in_MSpregnancy_trimesters.R")
+launch_step("p_steps/10_T3_21_aggregate_prevalence_of_exclusive_use_MSmeds_in_MSpregnancy_trimesters.R")
+launch_step("p_steps/11_T4_10_create_template_on_prevalence_of_use_MSdrugs_in_MSpregnancy_cohort.R")
