19df8c4523a5823dbf4af57b25072c824f53e127
Davide <davide.messina@ars.toscana.it>
2024-03-15 15:52:34 +0100

    Template 1 and 2 as specified in "data model" of codebooks (so not only in final table format)

--- p_steps/08_T4_20_create_descriptives_pregnancies.R
+++ p_steps/08_T4_20_create_descriptives_pregnancies.R
@@ -1,3 +1,9 @@
+##%######################################################%##
+#                                                          #
+####             GENERATE TEMPLATE 1 AND 2              ####
+#                                                          #
+##%######################################################%##
+
 smart_load("D3_DU_PREGNANCY_COHORT_variables", dirtemp, extension = extension)
 
 preg_cohort <- D3_DU_PREGNANCY_COHORT_variables[, .(person_id, pregnancy_id, number_of_pregnancies_in_the_study,
@@ -13,6 +19,16 @@ tmp2 <- preg_cohort[pregnancy_with_MS == 1, .(person_id, number_of_pregnancies_i
                                               pregnancy_start_date, pregnancy_end_date, strata = 2)]
 
 preg_cohort_strat <- rbindlist(list(tmp1, tmp2), use.names = T)
+
+# Initial part of first shell table
+D5_1 <- preg_cohort_strat[, .(n1 = .N), by = "strata"]
+D5_1_temp <- preg_cohort_strat[number_of_pregnancies_in_the_study == 1, .(n2 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p2 := n2 / n1 * 100]
+D5_1_temp <- preg_cohort_strat[number_of_pregnancies_in_the_study > 1, .(n3 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p3 := n3 / n1 * 100]
+
 
 header_string <- list(label = '',
                       stat_1 = '**Number of pregnancies in the Pregnancy cohort N (%)**',
@@ -31,14 +47,54 @@ tab1a <- preg_cohort_strat[, total := 1] %>%
 
 
 preg_cohort_strat[, strata := as.factor(strata)]
-preg_cohort_strat_mult <- preg_cohort_strat[number_of_pregnancies_in_the_study > 2, ]
+preg_cohort_strat_mult <- preg_cohort_strat[number_of_pregnancies_in_the_study > 1, ]
 setorder(preg_cohort_strat_mult, person_id, strata, pregnancy_start_date)
 # TODO ask if ceiling or floor is correct
-preg_cohort_strat_mult[, dist := ceiling(as.period(interval(shift(pregnancy_end_date), pregnancy_start_date)) / months(1)),
+preg_cohort_strat_mult[, dist := floor(as.integer(difftime(pregnancy_start_date, shift(pregnancy_end_date))) / 30.25),
                        by = c("person_id", "strata")]
 preg_cohort_strat_mult <- preg_cohort_strat_mult[!is.na(dist), ]
+
+# Second part of first shell table
+D5_1[, strata := as.factor(strata)]
+D5_1_temp <- preg_cohort_strat_mult[dist > 15, .(n4 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p4 := n4 / n3 * 100]
+D5_1_temp <- preg_cohort_strat_mult[dist <= 15 & dist > 12, .(n5 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p5 := n5 / n3 * 100]
+D5_1_temp <- preg_cohort_strat_mult[dist <= 12 & dist > 6, .(n6 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p6 := n6 / n3 * 100]
+D5_1_temp <- preg_cohort_strat_mult[dist <= 6 & dist > 3, .(n7 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p7 := n7 / n3 * 100]
+D5_1_temp <- preg_cohort_strat_mult[dist <= 3, .(n8 = .N), by = "strata"]
+D5_1 <- merge(D5_1, D5_1_temp, all.x = T)
+D5_1[, p8 := n8 / n3 * 100]
+
+D5_1[, strata := as.numeric(strata)]
+setnafill(D5_1, fill = 0)
+setnames(D5_1, "strata", "column_identifier")
+
+D5_1_mask <- D5_1[, lapply(.SD, as.character)]
+columns_to_mask_simple <- c("n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8")
+columns_to_mask_compl <- c("p2", "p3", "p4", "p5", "p6", "p7", "p8")
+for (col_mask in columns_to_mask_simple) {
+  
+  if (col_mask != "n1") {
+    col_to_mask <- columns_to_mask_compl[which(columns_to_mask_simple == col_mask) - 1]
+    D5_1_mask[between(as.numeric(get(col_mask)), 1, 4), (col_to_mask) := NA]
+  }
+  
+  D5_1_mask[between(as.numeric(get(col_mask)), 1, 4), (col_mask) := "<5"]
+}
+
+smart_save(D5_1, direxp, override_name = "D5_DU_for_Template_1", extension = extension, save_copy = "csv")
+smart_save(D5_1_mask, direxpmask, override_name = "D5_DU_for_Template_1_masked",
+           extension = extension, save_copy = "csv")
+
 # TODO Change this in regards to floor/ceiling above
-preg_cohort_strat_mult[, dist := cut(dist, c(0, 3, 6, 12, 15, Inf),
+preg_cohort_strat_mult[, dist := cut(dist, c(0, 3, 6, 12, 15, Inf), right = F,
                                      labels = c("Less than 3 months", "Between 3 and 6 months",
                                                 "Between 6 and 12 months", "Between 12 and 15 months",
                                                 "More than 15 months"))]
@@ -109,6 +165,29 @@ preg_cohort_filtered[, pregnancy_with_MS_detail := factor(pregnancy_with_MS_deta
                                                           labels = c("More than 12 months prior to pregnancy",
                                                                      "3-12 months prior to pregnancy",
                                                                      "0-3 months prior to pregnancy", "during pregnancy"))]
+
+# Second shell table
+D5_2 <- preg_cohort_filtered[, .(column_identifier = 1, n1 = .N)]
+D5_2_temp <- preg_cohort_filtered[pregnancy_with_MS_detail == "More than 12 months prior to pregnancy", .(n2 = .N)]
+D5_2 <- cbind(D5_2, D5_2_temp)
+D5_2_temp <- preg_cohort_filtered[pregnancy_with_MS_detail == "3-12 months prior to pregnancy", .(n3 = .N)]
+D5_2 <- cbind(D5_2, D5_2_temp)
+D5_2_temp <- preg_cohort_filtered[pregnancy_with_MS_detail == "0-3 months prior to pregnancy", .(n4 = .N)]
+D5_2 <- cbind(D5_2, D5_2_temp)
+D5_2_temp <- preg_cohort_filtered[pregnancy_with_MS_detail == "during pregnancy", .(n5 = .N)]
+D5_2 <- cbind(D5_2, D5_2_temp)
+
+setnafill(D5_2, fill = 0)
+
+D5_2_mask <- D5_2[, lapply(.SD, as.character)]
+columns_to_mask_simple <- c("n1", "n2", "n3", "n4", "n5")
+for (col_mask in columns_to_mask_simple) {
+  D5_2_mask[between(as.numeric(get(col_mask)), 1, 4), (col_mask) := "<5"]
+}
+
+smart_save(D5_1, direxp, override_name = "D5_DU_for_Template_2", extension = extension, save_copy = "csv")
+smart_save(D5_1_mask, direxpmask, override_name = "D5_DU_for_Template_2_masked",
+           extension = extension, save_copy = "csv")
 
 header_string <- list(label = '**Date of MS diagnosis**',
                       stat_0 = '**Number of pregnancies N (%)**')
