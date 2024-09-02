f00c370ff7a9f6f0a16f720b9bcff87089ddc331
Davide <davide.messina@ars.toscana.it>
2024-09-02 20:52:19 +0200

    DU pregnancy entry date

--- p_steps/09_T3_10_create_candidate_matches_MS_non_pregnant.R
+++ p_steps/09_T3_10_create_candidate_matches_MS_non_pregnant.R
@@ -17,8 +17,8 @@ ms_cohort_not_preg <- unique(D4_DU_MS_COHORT[person_id %not in% unique(preg_coho
                                                cohort_exit_date, start_candidate_period = pmax(cohort_entry_date, date_MS),
                                                end_candidate_period = cohort_exit_date)])
 
-preg_cohort[, c("cohort_entry_date", "cohort_exit_date") := list(min(cohort_entry_date), min(cohort_exit_date)),
-            by = "person_id"]
+# preg_cohort[, c("cohort_entry_date", "cohort_exit_date") := list(min(cohort_entry_date), min(cohort_exit_date)),
+#             by = "person_id"]
 
 setorder(preg_cohort, person_id, DU_pregnancy_study_entry_date)
 
@@ -55,8 +55,9 @@ initial_preg_cohort <- initial_preg_cohort[start_candidate_period < DU_pregnancy
 initial_preg_cohort[, end_candidate_period := DU_pregnancy_study_entry_date - 1]
 
 #lead start_date
-preg_cohort[, DU_pregnancy_study_entry_date_lead := shift(DU_pregnancy_study_entry_date, type = "lead",
-                                                          fill = cohort_exit_date[.N] + 1), by = "person_id"]
+preg_cohort[, DU_pregnancy_study_entry_date_lead := min(shift(DU_pregnancy_study_entry_date, type = "lead",
+                                                              fill = cohort_exit_date[.N] + 1),
+                                                        cohort_exit_date + 1), by = "person_id"]
 
 # Calculate start_candidate_period and end_candidate_period
 # KEEP <= since we may have periods of only 1 day
