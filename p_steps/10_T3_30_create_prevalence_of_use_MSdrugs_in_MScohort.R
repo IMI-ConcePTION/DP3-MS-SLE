4daf8040d2bbaaf5fe2b9b31df2d3f924fd42dd7
Davide <davide.messina@ars.toscana.it>
2024-10-31 16:19:37 +0100

    Table 6 and 7 not computed anymore for datasource with only pregnancies

--- p_steps/10_T3_30_create_prevalence_of_use_MSdrugs_in_MScohort.R
+++ p_steps/10_T3_30_create_prevalence_of_use_MSdrugs_in_MScohort.R
@@ -5,91 +5,95 @@
 #                                                          #
 ##%######################################################%##
 
-# Load dataset of pregnancies and medicines conceptsets
-smart_load("D4_DU_MS_COHORT", diroutput, extension = extension)
-
-medications <- rbindlist(lapply(concept_sets_of_our_study_DU, function(x) {
-  return(get(load(paste0(dirconceptsets, x, ".RData"))[[1]])[, .(person_id, date)][, concept := x])
-}))
-medications <- unique(medications)
-
-# Calculate yearly prevalence of use of each medication
-prevalence_of_use_yearly <- CountPrevalence(D4_DU_MS_COHORT, medications, c("person_id"), Start_date = "cohort_entry_date",
-                                            End_date = "cohort_exit_date", Birth_date = "birth_date",
-                                            Name_condition = "concept", Date_condition = "date", Type_prevalence = "of use",
-                                            Increment_period = "year",
-                                            Start_study_time = max(recommended_start_date, study_start),
-                                            End_study_time = study_end, Conditions = unique(medications[, concept]),
-                                            include_remaning_ages = F, Age_bands = ageband_definition_level_1,
-                                            Aggregate = F)
-
-# Extract year from timeframe and set CalendarTime level_order
-prevalence_of_use_yearly[, timeframe := as.integer(substr(timeframe, 1, 4))]
-prevalence_of_use_yearly[, CalendarTime_level_order := 1]
-
-# Calculate 5 years and whole period prevalence of use of each medication
-level_2_periods <- list(list("20050101", "20091231"),
-                        list("20100101", "20141231"),
-                        list("20150101", "20191231"))
-
-level_99_periods <- list(list("20050101", "20191231"))
-
-prevalence_of_use_5y_all <- CountPrevalence(D4_DU_MS_COHORT, medications, c("person_id"), Start_date = "cohort_entry_date",
-                                            End_date = "cohort_exit_date", Birth_date = "birth_date",
-                                            Name_condition = "concept", Date_condition = "date", Type_prevalence = "of use",
-                                            Periods_of_time = c(level_2_periods, level_99_periods),
-                                            Start_study_time = max(recommended_start_date, study_start),
-                                            End_study_time = study_end, Conditions = unique(medications[, concept]),
-                                            include_remaning_ages = F, Age_bands = ageband_definition_level_1,
-                                            Aggregate = F)
-
-# Extract year from timeframe and set CalendarTime level_order
-prevalence_of_use_5y_all[, timeframe := paste(year(as.Date(substr(timeframe, 1, 10))),
-                                              year(as.Date(substr(timeframe, 12, 21))), sep = "-")]
-prevalence_of_use_5y_all[, CalendarTime_level_order := fifelse(timeframe == "2005-2019", 99, 2)]
-
-# Combine prevalence datasets
-prevalence_of_use <- rbindlist(list(prevalence_of_use_yearly, prevalence_of_use_5y_all))
-
-# Keep only periods when persons are in population and remove unnecessary columns
-prevalence_of_use <- prevalence_of_use[in_population != 0, ]
-prevalence_of_use[, c("cohort_entry_date", "cohort_exit_date") := NULL, ]
-
-# Modify column names and add missing medications (as 0)
-cols_to_add <- setdiff(paste0("prev_", concept_sets_of_our_study_DU), colnames(prevalence_of_use))
-prevalence_of_use[, (cols_to_add) := 0L]
-setnames(prevalence_of_use, paste0("prev_", concept_sets_of_our_study_DU), concept_sets_of_our_study_DU)
-prevalence_of_use[ , (concept_sets_of_our_study_DU) := lapply(.SD, as.integer), .SDcols = concept_sets_of_our_study_DU]
-
-# Create the category anydrug
-prevalence_of_use[, anydrug := do.call(pmax, .SD), .SDcols = concept_sets_of_our_study_DU]
-
-# Melt the medications
-prevalence_of_use <- melt(prevalence_of_use, measure.vars = c(concept_sets_of_our_study_DU, "anydrug"),
-                          variable.name = "medication")
-
-# Calculate manually the total irrespective by agebands
-prevalence_of_use_allages <- unique(copy(prevalence_of_use)[, Ageband := NULL])
-prevalence_of_use_allages <- prevalence_of_use_allages[, .(in_population = sum(in_population), value = sum(value)),
-                                                       by = c("timeframe", "medication", "CalendarTime_level_order")]
-prevalence_of_use_allages[, Ageband := "allAgeband"]
-
-# Calculate manually the total by timeframe, medication and ageband
-prevalence_of_use <- prevalence_of_use[, .(in_population = sum(in_population), value = sum(value)),
-                                       by = c("timeframe", "medication", "Ageband", "CalendarTime_level_order")]
-
-# Combine previous datasets to get aggregate dataset with correct totals for ageband "allAgeband"
-# Add Ageband level_order and medication level_order
-prevalence_of_use <- rbindlist(list(prevalence_of_use[, Ageband_level_order := 1],
-                                    prevalence_of_use_allages[, Ageband_level_order := 99]), use.names = T)
-prevalence_of_use[, medication_level_order := fifelse(medication == "anydrug", 99, 1)]
-
-# Cleaned name and order of columns
-prevalence_of_use <- prevalence_of_use[, .(medication_label = medication, medication_level_order,
-                                           CalendarTime_label = timeframe, CalendarTime_level_order,
-                                           Ageband_label = Ageband, Ageband_level_order, numerator = value,
-                                           denominator = in_population)]
-
-# Save the file
-smart_save(prevalence_of_use, diroutput, override_name = "D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
-           extension = extension, save_copy = "csv")
+if (thisdatasource %not in% datasources_only_preg) {
+  
+  # Load dataset of pregnancies and medicines conceptsets
+  smart_load("D4_DU_MS_COHORT", diroutput, extension = extension)
+  
+  medications <- rbindlist(lapply(concept_sets_of_our_study_DU, function(x) {
+    return(get(load(paste0(dirconceptsets, x, ".RData"))[[1]])[, .(person_id, date)][, concept := x])
+  }))
+  medications <- unique(medications)
+  
+  # Calculate yearly prevalence of use of each medication
+  prevalence_of_use_yearly <- CountPrevalence(D4_DU_MS_COHORT, medications, c("person_id"), Start_date = "cohort_entry_date",
+                                              End_date = "cohort_exit_date", Birth_date = "birth_date",
+                                              Name_condition = "concept", Date_condition = "date", Type_prevalence = "of use",
+                                              Increment_period = "year",
+                                              Start_study_time = max(recommended_start_date, study_start),
+                                              End_study_time = study_end, Conditions = unique(medications[, concept]),
+                                              include_remaning_ages = F, Age_bands = ageband_definition_level_1,
+                                              Aggregate = F)
+  
+  # Extract year from timeframe and set CalendarTime level_order
+  prevalence_of_use_yearly[, timeframe := as.integer(substr(timeframe, 1, 4))]
+  prevalence_of_use_yearly[, CalendarTime_level_order := 1]
+  
+  # Calculate 5 years and whole period prevalence of use of each medication
+  level_2_periods <- list(list("20050101", "20091231"),
+                          list("20100101", "20141231"),
+                          list("20150101", "20191231"))
+  
+  level_99_periods <- list(list("20050101", "20191231"))
+  
+  prevalence_of_use_5y_all <- CountPrevalence(D4_DU_MS_COHORT, medications, c("person_id"), Start_date = "cohort_entry_date",
+                                              End_date = "cohort_exit_date", Birth_date = "birth_date",
+                                              Name_condition = "concept", Date_condition = "date", Type_prevalence = "of use",
+                                              Periods_of_time = c(level_2_periods, level_99_periods),
+                                              Start_study_time = max(recommended_start_date, study_start),
+                                              End_study_time = study_end, Conditions = unique(medications[, concept]),
+                                              include_remaning_ages = F, Age_bands = ageband_definition_level_1,
+                                              Aggregate = F)
+  
+  # Extract year from timeframe and set CalendarTime level_order
+  prevalence_of_use_5y_all[, timeframe := paste(year(as.Date(substr(timeframe, 1, 10))),
+                                                year(as.Date(substr(timeframe, 12, 21))), sep = "-")]
+  prevalence_of_use_5y_all[, CalendarTime_level_order := fifelse(timeframe == "2005-2019", 99, 2)]
+  
+  # Combine prevalence datasets
+  prevalence_of_use <- rbindlist(list(prevalence_of_use_yearly, prevalence_of_use_5y_all))
+  
+  # Keep only periods when persons are in population and remove unnecessary columns
+  prevalence_of_use <- prevalence_of_use[in_population != 0, ]
+  prevalence_of_use[, c("cohort_entry_date", "cohort_exit_date") := NULL, ]
+  
+  # Modify column names and add missing medications (as 0)
+  cols_to_add <- setdiff(paste0("prev_", concept_sets_of_our_study_DU), colnames(prevalence_of_use))
+  prevalence_of_use[, (cols_to_add) := 0L]
+  setnames(prevalence_of_use, paste0("prev_", concept_sets_of_our_study_DU), concept_sets_of_our_study_DU)
+  prevalence_of_use[ , (concept_sets_of_our_study_DU) := lapply(.SD, as.integer), .SDcols = concept_sets_of_our_study_DU]
+  
+  # Create the category anydrug
+  prevalence_of_use[, anydrug := do.call(pmax, .SD), .SDcols = concept_sets_of_our_study_DU]
+  
+  # Melt the medications
+  prevalence_of_use <- melt(prevalence_of_use, measure.vars = c(concept_sets_of_our_study_DU, "anydrug"),
+                            variable.name = "medication")
+  
+  # Calculate manually the total irrespective by agebands
+  prevalence_of_use_allages <- unique(copy(prevalence_of_use)[, Ageband := NULL])
+  prevalence_of_use_allages <- prevalence_of_use_allages[, .(in_population = sum(in_population), value = sum(value)),
+                                                         by = c("timeframe", "medication", "CalendarTime_level_order")]
+  prevalence_of_use_allages[, Ageband := "allAgeband"]
+  
+  # Calculate manually the total by timeframe, medication and ageband
+  prevalence_of_use <- prevalence_of_use[, .(in_population = sum(in_population), value = sum(value)),
+                                         by = c("timeframe", "medication", "Ageband", "CalendarTime_level_order")]
+  
+  # Combine previous datasets to get aggregate dataset with correct totals for ageband "allAgeband"
+  # Add Ageband level_order and medication level_order
+  prevalence_of_use <- rbindlist(list(prevalence_of_use[, Ageband_level_order := 1],
+                                      prevalence_of_use_allages[, Ageband_level_order := 99]), use.names = T)
+  prevalence_of_use[, medication_level_order := fifelse(medication == "anydrug", 99, 1)]
+  
+  # Cleaned name and order of columns
+  prevalence_of_use <- prevalence_of_use[, .(medication_label = medication, medication_level_order,
+                                             CalendarTime_label = timeframe, CalendarTime_level_order,
+                                             Ageband_label = Ageband, Ageband_level_order, numerator = value,
+                                             denominator = in_population)]
+  
+  # Save the file
+  smart_save(prevalence_of_use, diroutput, override_name = "D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
+             extension = extension, save_copy = "csv")
+  
+}
--- p_steps/11_T4_30_create_template_on_prevalence_of_use_MSdrugs_in_MS_cohort.R
+++ p_steps/11_T4_30_create_template_on_prevalence_of_use_MSdrugs_in_MS_cohort.R
@@ -4,71 +4,74 @@
 #                                                          #
 ##%######################################################%##
 
-# Load dataset of pregnancies and medicines conceptsets
-medication_preg <- smart_load("D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
-                              diroutput, extension = extension, return = T)
-
-# Select necessary levels
-medication_preg <- medication_preg[CalendarTime_level_order %in% c(1, 99) & Ageband_level_order == 99, ]
-
-# Recode 2005-2019 to "all"
-medication_preg <- medication_preg[CalendarTime_level_order == 99, CalendarTime_label := "all"]
-
-# Change names of columns
-medication_preg <- medication_preg[, .(row_identifier_1 = medication_label, row_identifier_2 = CalendarTime_label,
-                                       n1 = numerator, n2 = denominator, row_identifier_1_order = medication_level_order,
-                                       row_identifier_2_order = CalendarTime_level_order)]
-
-# Calculate prevalence and CI
-medication_preg[, c("n3", "n4", "n5") := lapply(Hmisc::binconf(n1, n2, return.df = T) * 1000, round, 10)]
-
-# Reorder columns
-setcolorder(medication_preg, c("row_identifier_1", "row_identifier_2", "n1", "n2", "n3", "n4", "n5",
-                               "row_identifier_1_order", "row_identifier_2_order"))
-
-# Generate masked dataset
-medication_preg_mask <- copy(medication_preg)[, n1 := as.character(n1)]
-medication_preg_mask[, n2 := as.character(n2)]
-medication_preg_mask[as.integer(n1) > 0 & as.integer(n1) < 5, c("n1", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
-medication_preg_mask[as.integer(n2) > 0 & as.integer(n2) < 5, c("n2", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
-
-# Save the file
-smart_save(medication_preg, direxp, override_name = "D5_DU_for_Template_6", extension = extension, save_copy = "csv")
-smart_save(medication_preg_mask, direxpmask, override_name = "D5_DU_for_Template_6_masked",
-           extension = extension, save_copy = "csv")
-smart_save(medication_preg_mask, direxpred, override_name = "D5_DU_for_Template_6_masked",
-           extension = extension, save_copy = "csv")
-
-# Load dataset of pregnancies and medicines conceptsets
-medication_preg <- smart_load("D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
-                              diroutput, extension = extension, return = T)
-
-# Select necessary levels
-medication_preg <- medication_preg[CalendarTime_level_order %in% c(2, 99), ]
-
-# Change names of columns
-medication_preg <- medication_preg[, .(row_identifier_1 = medication_label, row_identifier_2 = CalendarTime_label,
-                                       row_identifier_3 = Ageband_label, n1 = numerator, n2 = denominator,
-                                       row_identifier_1_order = medication_level_order,
-                                       row_identifier_2_order = CalendarTime_level_order,
-                                       row_identifier_3_order = Ageband_level_order)]
-
-# Calculate prevalence and CI
-medication_preg[, c("n3", "n4", "n5") := lapply(Hmisc::binconf(n1, n2, return.df = T) * 1000, round, 10)]
-
-# Reorder columns
-setcolorder(medication_preg, c("row_identifier_1", "row_identifier_2", "row_identifier_3", "n1", "n2", "n3", "n4", "n5",
-                               "row_identifier_1_order", "row_identifier_2_order", "row_identifier_3_order"))
-
-# Generate masked dataset
-medication_preg_mask <- copy(medication_preg)[, n1 := as.character(n1)]
-medication_preg_mask[, n2 := as.character(n2)]
-medication_preg_mask[as.integer(n1) > 0 & as.integer(n1) < 5, c("n1", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
-medication_preg_mask[as.integer(n2) > 0 & as.integer(n2) < 5, c("n2", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
-
-# Save the file
-smart_save(medication_preg, direxp, override_name = "D5_DU_for_Template_7", extension = extension, save_copy = "csv")
-smart_save(medication_preg_mask, direxpmask, override_name = "D5_DU_for_Template_7_masked",
-           extension = extension, save_copy = "csv")
-smart_save(medication_preg_mask, direxpred, override_name = "D5_DU_for_Template_7_masked",
-           extension = extension, save_copy = "csv")
+if (thisdatasource %not in% datasources_only_preg) {
+  
+  # Load dataset of pregnancies and medicines conceptsets
+  medication_preg <- smart_load("D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
+                                diroutput, extension = extension, return = T)
+  
+  # Select necessary levels
+  medication_preg <- medication_preg[CalendarTime_level_order %in% c(1, 99) & Ageband_level_order == 99, ]
+  
+  # Recode 2005-2019 to "all"
+  medication_preg <- medication_preg[CalendarTime_level_order == 99, CalendarTime_label := "all"]
+  
+  # Change names of columns
+  medication_preg <- medication_preg[, .(row_identifier_1 = medication_label, row_identifier_2 = CalendarTime_label,
+                                         n1 = numerator, n2 = denominator, row_identifier_1_order = medication_level_order,
+                                         row_identifier_2_order = CalendarTime_level_order)]
+  
+  # Calculate prevalence and CI
+  medication_preg[, c("n3", "n4", "n5") := lapply(Hmisc::binconf(n1, n2, return.df = T) * 1000, round, 10)]
+  
+  # Reorder columns
+  setcolorder(medication_preg, c("row_identifier_1", "row_identifier_2", "n1", "n2", "n3", "n4", "n5",
+                                 "row_identifier_1_order", "row_identifier_2_order"))
+  
+  # Generate masked dataset
+  medication_preg_mask <- copy(medication_preg)[, n1 := as.character(n1)]
+  medication_preg_mask[, n2 := as.character(n2)]
+  medication_preg_mask[as.integer(n1) > 0 & as.integer(n1) < 5, c("n1", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
+  medication_preg_mask[as.integer(n2) > 0 & as.integer(n2) < 5, c("n2", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
+  
+  # Save the file
+  smart_save(medication_preg, direxp, override_name = "D5_DU_for_Template_6", extension = extension, save_copy = "csv")
+  smart_save(medication_preg_mask, direxpmask, override_name = "D5_DU_for_Template_6_masked",
+             extension = extension, save_copy = "csv")
+  smart_save(medication_preg_mask, direxpred, override_name = "D5_DU_for_Template_6_masked",
+             extension = extension, save_copy = "csv")
+  
+  # Load dataset of pregnancies and medicines conceptsets
+  medication_preg <- smart_load("D4_DU_prevalence_of_use_MSdrugs_in_MS_cohort",
+                                diroutput, extension = extension, return = T)
+  
+  # Select necessary levels
+  medication_preg <- medication_preg[CalendarTime_level_order %in% c(2, 99), ]
+  
+  # Change names of columns
+  medication_preg <- medication_preg[, .(row_identifier_1 = medication_label, row_identifier_2 = CalendarTime_label,
+                                         row_identifier_3 = Ageband_label, n1 = numerator, n2 = denominator,
+                                         row_identifier_1_order = medication_level_order,
+                                         row_identifier_2_order = CalendarTime_level_order,
+                                         row_identifier_3_order = Ageband_level_order)]
+  
+  # Calculate prevalence and CI
+  medication_preg[, c("n3", "n4", "n5") := lapply(Hmisc::binconf(n1, n2, return.df = T) * 1000, round, 10)]
+  
+  # Reorder columns
+  setcolorder(medication_preg, c("row_identifier_1", "row_identifier_2", "row_identifier_3", "n1", "n2", "n3", "n4", "n5",
+                                 "row_identifier_1_order", "row_identifier_2_order", "row_identifier_3_order"))
+  
+  # Generate masked dataset
+  medication_preg_mask <- copy(medication_preg)[, n1 := as.character(n1)]
+  medication_preg_mask[, n2 := as.character(n2)]
+  medication_preg_mask[as.integer(n1) > 0 & as.integer(n1) < 5, c("n1", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
+  medication_preg_mask[as.integer(n2) > 0 & as.integer(n2) < 5, c("n2", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
+  
+  # Save the file
+  smart_save(medication_preg, direxp, override_name = "D5_DU_for_Template_7", extension = extension, save_copy = "csv")
+  smart_save(medication_preg_mask, direxpmask, override_name = "D5_DU_for_Template_7_masked",
+             extension = extension, save_copy = "csv")
+  smart_save(medication_preg_mask, direxpred, override_name = "D5_DU_for_Template_7_masked",
+             extension = extension, save_copy = "csv")
+}
