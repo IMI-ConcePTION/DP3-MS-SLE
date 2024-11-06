c20f018c954cfc5583894214b200e0ff55885f12
Davide Messina <davide.messina@ars.toscana.it>
2024-11-06 19:25:52 +0100

    Revised filtering for simplified dataset

--- p_steps/11_T4_40_create_template_on_prevalence_of_use_MSdrugs_in_MSpregnancy_cohort.R
+++ p_steps/11_T4_40_create_template_on_prevalence_of_use_MSdrugs_in_MSpregnancy_cohort.R
@@ -27,7 +27,7 @@ prev_MS_preg_cohort[, column_identifier := factor(column_identifier,
                                                   levels = c("pre_1", "pre_2", "pre_3", "pre_4", "all_pre", "during_1",
                                                              "during_2", "during_3", "all_during", "after_1"))]
 
-setorder(prev_MS_preg_cohort, is_pregnancy, row_identifier_1, row_identifier_2_order, row_identifier_2,
+setorder(prev_MS_preg_cohort, is_pregnancy, row_identifier_1_order, row_identifier_1, row_identifier_2_order, row_identifier_2,
          row_identifier_3_order, row_identifier_3, column_identifier)
 
 # Generate masked dataset
@@ -40,41 +40,47 @@ smart_save(prev_MS_preg_cohort, direxp, override_name = "D5_DU_for_Templates_8_1
 smart_save(prev_MS_preg_cohort_mask, direxpmask, override_name = "D5_DU_for_Templates_8_11_masked",
            extension = extension, save_copy = "csv")
 
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_mask[!(n2 == "0" | row_identifier_2_order == 1), ]
+prev_MS_preg_cohort <- prev_MS_preg_cohort[!(n2 == 0 | row_identifier_2_order == 1), ]
 
 # 0<x<15 in all age -> Do not stratify by ageband
-# 5Y period small number -> not stratify by 5y periods and ageband
+prev_MS_preg_cohort_flag <- copy(prev_MS_preg_cohort)[row_identifier_3_order == 99 & row_identifier_2_order != 1, ]
+prev_MS_preg_cohort_flag[, flag := fifelse(n2 < 15 & n2 >= 0, 1, 0)]
+prev_MS_preg_cohort_flag <- unique(prev_MS_preg_cohort_flag[, c("row_identifier_3", "row_identifier_3_order") := NULL])
+prev_MS_preg_cohort_to_retain_1 <- prev_MS_preg_cohort_flag[copy(prev_MS_preg_cohort),
+                                 on = c("is_pregnancy", "row_identifier_1", "row_identifier_2", "column_identifier",
+                                        "n1", "n2", "n3", "n4", "n5", "row_identifier_1_order", "row_identifier_2_order")]
+prev_MS_preg_cohort_to_retain_1 <- prev_MS_preg_cohort_to_retain_1[row_identifier_3_order == 99, flag := 0][is.na(flag) | flag == 0, ][, flag := NULL]
+
 # 0<x<5 in one ageband -> Do not stratify by ageband
+prev_MS_preg_cohort_flag <- copy(prev_MS_preg_cohort)[row_identifier_3_order == 1, ]
+prev_MS_preg_cohort_flag[, flag := fifelse(n2 < 5 & n2 >= 0, 1, 0)]
+prev_MS_preg_cohort_flag <- unique(prev_MS_preg_cohort_flag[, c("row_identifier_3") := NULL])
+prev_MS_preg_cohort_to_retain_2 <- prev_MS_preg_cohort_flag[copy(prev_MS_preg_cohort),
+                                                         on = c("is_pregnancy", "row_identifier_1", "row_identifier_2", "column_identifier",
+                                                                "n1", "n2", "n3", "n4", "n5", "row_identifier_1_order", "row_identifier_2_order", "row_identifier_3_order")]
+prev_MS_preg_cohort_to_retain_2 <- prev_MS_preg_cohort_to_retain_2[is.na(flag) | flag == 0, ][, flag := NULL]
 
-prev_MS_preg_cohort_to_remove_1 <- prev_MS_preg_cohort[row_identifier_2_order != 1 & row_identifier_3_order == 99 &
-                                                         n2 < 15 & n2 >= 0, ]
-prev_MS_preg_cohort_to_remove_2 <- prev_MS_preg_cohort[row_identifier_3_order == 1 & n2 < 5 & n2 >= 0, ]
-prev_MS_preg_cohort_to_remove_3 <- prev_MS_preg_cohort[row_identifier_2_order == 2 & n2 < 5 & n2 >= 0, ]
-prev_MS_preg_cohort_to_remove <- rbindlist(list(prev_MS_preg_cohort_to_remove_1, prev_MS_preg_cohort_to_remove_2,
-                                                prev_MS_preg_cohort_to_remove_3))
-prev_MS_preg_cohort_to_remove <- prev_MS_preg_cohort_to_remove[, .(is_pregnancy, row_identifier_1, row_identifier_2,
-                                                                   row_identifier_3,column_identifier)]
-prev_MS_preg_cohort_to_remove[, flag := 1]
+# 5Y period small number -> not stratify by 5y periods and ageband
+prev_MS_preg_cohort_flag <- copy(prev_MS_preg_cohort)[row_identifier_2_order == 2, ]
+prev_MS_preg_cohort_flag[, flag := fifelse(n2 < 5 & n2 >= 0, 1, 0)]
+prev_MS_preg_cohort_flag <- unique(prev_MS_preg_cohort_flag[, c("row_identifier_2", "row_identifier_3", "row_identifier_3_order") := NULL])
+prev_MS_preg_cohort_to_retain_3 <- prev_MS_preg_cohort_flag[copy(prev_MS_preg_cohort),
+                                                         on = c("is_pregnancy", "row_identifier_1", "column_identifier",
+                                                                "n1", "n2", "n3", "n4", "n5", "row_identifier_1_order", "row_identifier_2_order")]
+prev_MS_preg_cohort_to_retain_3 <- prev_MS_preg_cohort_to_retain_3[is.na(flag) | flag == 0, ][, flag := NULL]
 
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_to_remove[prev_MS_preg_cohort_mask,
-                                                on = c("is_pregnancy", "row_identifier_1", "row_identifier_2",
-                                                       "row_identifier_3", "column_identifier")]
-prev_MS_preg_cohort_mask[is.na(flag), flag := 0]
-prev_MS_preg_cohort_mask[, flag_max := max(flag), by = c("is_pregnancy", "row_identifier_1", "row_identifier_2",
-                                                     "column_identifier")]
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_mask[row_identifier_3_order == 1 & flag_max == 1, flag_total := 1]
+prev_MS_preg_cohort_filtered <- prev_MS_preg_cohort_to_retain_1[copy(prev_MS_preg_cohort), on = colnames(prev_MS_preg_cohort), nomatch = NULL]
+prev_MS_preg_cohort_filtered <- prev_MS_preg_cohort_to_retain_2[prev_MS_preg_cohort_filtered, on = colnames(prev_MS_preg_cohort), nomatch = NULL]
+prev_MS_preg_cohort_filtered <- prev_MS_preg_cohort_to_retain_3[prev_MS_preg_cohort_filtered, on = colnames(prev_MS_preg_cohort), nomatch = NULL]
 
-prev_MS_preg_cohort_mask[, flag_max := max(flag), by = c("is_pregnancy", "row_identifier_1", "row_identifier_3",
-                                                         "column_identifier")]
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_mask[, flag_total := fifelse(row_identifier_2_order == 2 & flag_max == 1, 1, flag_total)]
+setcolorder(prev_MS_preg_cohort_filtered, c("is_pregnancy", "row_identifier_1", "row_identifier_2", "row_identifier_3",
+                                   "column_identifier", "n1", "n2", "n3", "n4", "n5", "row_identifier_1_order",
+                                   "row_identifier_2_order", "row_identifier_3_order"))
 
-prev_MS_preg_cohort_mask[, flag_max := max(flag), by = c("is_pregnancy", "row_identifier_1", "row_identifier_2",
-                                                         "column_identifier")]
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_mask[, flag_total := fifelse(row_identifier_2_order != 1 & flag_max == 1, 1, flag_total)]
+# Generate masked dataset
+prev_MS_preg_cohort_filtered[, n1 := as.character(n1)][, n2 := as.character(n2)]
+prev_MS_preg_cohort_filtered[as.integer(n1) > 0 & as.integer(n1) < 5, c("n1", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
+prev_MS_preg_cohort_filtered[as.integer(n2) > 0 & as.integer(n2) < 5, c("n2", "n3", "n4", "n5") := list("<5", NA, NA, NA)]
 
-prev_MS_preg_cohort_mask[is.na(flag_total), flag := 0]
-prev_MS_preg_cohort_mask <- prev_MS_preg_cohort_mask[flag == 0, ]
-prev_MS_preg_cohort_mask[, c("flag", "flag_max", "flag_total") := NULL]
-
-smart_save(prev_MS_preg_cohort_mask, direxpred, override_name = "D5_DU_for_Templates_8_11_masked_simplified",
+smart_save(prev_MS_preg_cohort_filtered, direxpred, override_name = "D5_DU_for_Templates_8_11_masked_simplified",
            extension = extension, save_copy = "csv")
